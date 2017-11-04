bfbcf34d0bcb2979b8fc08790ef4393df9b7cbe6
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-11-04 20:23:53 +0800

    Remove outdated scripts (#4)

--- script/c511002759.lua
+++
@@ -1,9 +0,0 @@
---ダブルフィン・シャーク
-function c511002759.initial_effect(c)
-	c511002759.xyzlimit2=function(mc) return mc:IsAttribute(ATTRIBUTE_WATER) end
-	--
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(511001225)
-	c:RegisterEffect(e2)
-end
