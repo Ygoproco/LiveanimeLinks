460f7b7bbbb6dbc5aadefac596fd2028be01c178
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-11-05 08:00:08 -0200

    small fix (#8)
    * small fix
    
    removed debug message
    
    * fix
    
    wrong function

---
+++ script/c511004423.lua
@@ -0,0 +1,36 @@
+--Odd-Eyes Phantasma Dragon
+--cleaned up by MLD
+function c511004423.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(511004423)
+	e2:SetCondition(c511004423.spcon)
+	c:RegisterEffect(e2)
+	--attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetOperation(c511004423.op)
+	c:RegisterEffect(e3)
+end
+function c511004423.spcon(e)
+	return Duel.IsExistingMatchingCard(Card.IsSetCard,e:GetHandlerPlayer(),LOCATION_EXTRA,0,1,nil,0x99)
+end
+function c511004423.filter(c)
+	return c:IsFaceup() and c:IsType(TYPE_PENDULUM)
+end
+function c511004423.op(e,tp,eg,ev,ep,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	local ct=Duel.GetMatchingGroupCount(c511004423.filter,tp,LOCATION_EXTRA,0,nil)*1000
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-ct)
+		tc:RegisterEffect(e1)
+		tc=g:GetNext()
+	end
+end
