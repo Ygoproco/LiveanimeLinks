fafbba775c24e77d967b887338f6f94d75a83923
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-11-07 14:27:02 +0800

    Alert Lancer

---
+++ script/c511009621.lua
@@ -0,0 +1,20 @@
+--Alert Lancer
+--scripted by MLD
+function c511009621.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(c511009621.activate)
+	c:RegisterEffect(e1)
+end
+function c511009621.activate(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_PIERCE)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_CYBERSE))
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
