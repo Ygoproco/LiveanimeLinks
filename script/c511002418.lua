8e7d10e874b835ab18d533f67c60b29c26203455
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-11-25 22:14:30 +0800

    Indentation

--- script/c511002418.lua
+++ script/c511002418.lua
@@ -47,7 +47,7 @@ function c511002418.initial_effect(c)
 	e5:SetTarget(c511002418.destg)
 	e5:SetOperation(c511002418.desop)
 	c:RegisterEffect(e5)
-  --Double Snare
+	--Double Snare
 	local e6=Effect.CreateEffect(c)
 	e6:SetType(EFFECT_TYPE_SINGLE)
 	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SINGLE_RANGE)
