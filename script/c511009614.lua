86a01ba498af5a10eccb8bb8d9252a87a83a816f
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-12-22 14:47:40 +0800

    VRAINS fixes

--- script/c511009614.lua
+++ script/c511009614.lua
@@ -1,4 +1,5 @@
 --Helixx Marmotroll
+--fixed by MLD
 function c511009614.initial_effect(c)
 	--summon with no tribute
 	local e1=Effect.CreateEffect(c)
@@ -7,46 +8,35 @@ function c511009614.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SUMMON_PROC)
 	e1:SetCondition(c511009614.ntcon)
-	e1:SetCost(c511009614.ntcost)
+	e1:SetOperation(c511009614.ntop)
 	c:RegisterEffect(e1)
-	
-	--damage conversion
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetCode(EFFECT_REVERSE_DAMAGE)
-	e2:SetTargetRange(1,0)
-	e2:SetValue(c511009614.valcon)
-	e2:SetReset(RESET_PHASE+PHASE_END)
+	--battle damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e2:SetCondition(c511009614.damcon)
+	e2:SetOperation(c511009614.damop)
+	e2:SetCountLimit(1)
 	c:RegisterEffect(e2)
 end
 function c511009614.ntcon(e,c,minc)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	return minc==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
-		and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+	return minc==0 and c:GetLevel()>4 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,LOCATION_MZONE)==0
+		and Duel.IsExistingMatchingCard(c511009614.cfilter,tp,LOCATION_HAND,0,1,nil)
 end
 function c511009614.cfilter(c)
 	return c:IsSetCard(0x573) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end
-function c511009614.ntcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c511009614.cfilter,tp,LOCATION_HAND,0,1,nil) end
+function c511009614.ntop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,c511009614.cfilter,tp,LOCATION_HAND,0,1,1,nil)
 	Duel.SendtoGrave(g,REASON_COST)
 end
-
-function c511009614.valcon(e,re,r,rp,rc)
-	local c=e:GetHandler()
-	if bit.band(r,REASON_BATTLE)~=0 then
-		local tp=e:GetHandlerPlayer()
-		local bc=rc:GetBattleTarget()
-		if bc and bc==
-			and c:GetFlagEffect(511009614)==0 then
-			c:RegisterFlagEffect(511009614,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
-			return true
-		end
-	end
-	return false
+function c511009614.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetBattleDamage(tp)>0
+end
+function c511009614.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeBattleDamage(tp,0)
 end
--- script/c511009656.lua
+++ script/c511009656.lua
@@ -1,14 +1,13 @@
 --Dark Mummy Infuser
+--fixed by MLD
 function c511009656.initial_effect(c)
---search
+	--search
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(511009656,1))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetTarget(c511009656.tg)
-	e1:SetCondition(c511009656.spcon)
 	e1:SetOperation(c511009656.op)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
@@ -24,41 +23,29 @@ function c511009656.initial_effect(c)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
 end
-function c511009656.lkfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_LINK) and c:IsRace(RACE_ZOMBIE)
-end
-function c511009656.spcon(e,tp,eg,ep,ev,re,r,rp)
-	local zone=0
-	local lg=Duel.GetMatchingGroup(c511009656.lkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	for tc in aux.Next(lg) do
-		zone=bit.bor(zone,tc:GetLinkedZone())
-	end
-	return eg:IsContains(e:GetHandler())
-end
 function c511009656.filter(c,ec)
-	return c:IsFaceup() and c:IsType(TYPE_LINK)  and c:GetLinkedGroup():IsContains(ec)
-	-- ((ec==c and c:GetFlagEffect(55063681)==0) or (ec~=c and not ec:IsHasCardTarget(c)))
+	return c:IsFaceup() and c:IsType(TYPE_LINK) and c:GetLinkedGroup():IsContains(ec)
 end
-function c511009656.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c511009656.filter(chkc,e:GetHandler()) end
-	if chk==0 then return Duel.IsExistingTarget(c511009656.filter,tp,LOCATION_MZONE,0,1,nil,e:GetHandler()) end
+function c511009656.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c511009656.filter(chkc,e:GetHandler()) end
+	if chk==0 then return Duel.IsExistingTarget(c511009656.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e:GetHandler()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c511009656.filter,tp,LOCATION_MZONE,0,1,1,nil,e:GetHandler())
+	Duel.SelectTarget(tp,c511009656.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,e:GetHandler())
 end
 function c511009656.op(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local c=e:GetHandler()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) and c:IsFaceup() and c:IsRelateToEffect(e) then
-			c:SetCardTarget(tc)
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-			e1:SetRange(LOCATION_MZONE)
-			e1:SetCode(EFFECT_IMMUNE_EFFECT)
-			e1:SetValue(c511009656.efilter)
-			e1:SetCondition(c511009656.atkcon)
-			e1:SetReset(RESET_EVENT+0x1fe0000)
-			tc:RegisterEffect(e1)
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and c:IsFaceup() and c:IsRelateToEffect(e) then
+		c:SetCardTarget(tc)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_IMMUNE_EFFECT)
+		e1:SetValue(c511009656.efilter)
+		e1:SetCondition(c511009656.atkcon)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
 	end
 end
 function c511009656.efilter(e,te)
