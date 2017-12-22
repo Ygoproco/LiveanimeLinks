dd87f6517a1e7c1df6f70cbb436d37ca0fcd4649
ClementLouis <clementlouis.gallier@viacesi.fr>
2017-12-22 09:47:49 +0100

    Dark Mummy Infuser

--- script/c511009656.lua
+++ script/c511009656.lua
@@ -7,9 +7,9 @@ function c511009656.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetTarget(c511009656.tg)
-	e1:SetCondition(c511009656.spcon)
-	e1:SetOperation(c511009656.op)
+	e1:SetTarget(c511009656.target)
+	e1:SetCondition(c511009656.condition)
+	e1:SetOperation(c511009656.operation)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -27,7 +27,7 @@ end
 function c511009656.lkfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_LINK) and c:IsRace(RACE_ZOMBIE)
 end
-function c511009656.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c511009656.condition(e,tp,eg,ep,ev,re,r,rp)
 	local zone=0
 	local lg=Duel.GetMatchingGroup(c511009656.lkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	for tc in aux.Next(lg) do
@@ -45,7 +45,7 @@ function c511009656.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,c511009656.filter,tp,LOCATION_MZONE,0,1,1,nil,e:GetHandler())
 end
-function c511009656.op(e,tp,eg,ep,ev,re,r,rp)
+function c511009656.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local c=e:GetHandler()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) and c:IsFaceup() and c:IsRelateToEffect(e) then
