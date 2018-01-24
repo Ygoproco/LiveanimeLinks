3635a63be6afbaa37a076c5e20f69ed137d42e95
Larry126 <larry126@users.noreply.github.com>
2018-01-24 19:11:15 +0000

    More vrains anime

---
+++ script/c511600055.lua
@@ -0,0 +1,71 @@
+--マルチ・スレッジ・ハンマー (Anime)
+--Multi Sledgehammer (Anime)
+--scripted by Larry126
+function c511600055.initial_effect(c)
+	aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_CYBERSE),2,2)
+	c:EnableReviveLimit()
+	c:EnableCounterPermit(0x55)
+	--direct attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DIRECT_ATTACK)
+	e1:SetCondition(c511600055.dircon)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(71071546,0))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_BATTLED)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(c511600055.ctcon)
+	e2:SetTarget(c511600055.cttg)
+	e2:SetOperation(c511600055.ctop)
+	c:RegisterEffect(e2)
+	--tohand
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(51644030,0))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetCost(c511600055.atkcost)
+	e3:SetOperation(c511600055.atkop)
+	c:RegisterEffect(e3)
+end
+function c511600055.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:GetCounter(0x55)>0 end
+	local ct=c:GetCounter(0x55)
+	e:SetLabel(ct*1000)
+	c:RemoveCounter(tp,0x55,ct,REASON_COST)
+end
+function c511600055.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e1:SetValue(e:GetLabel())
+		c:RegisterEffect(e1)
+	end
+end
+function c511600055.dirfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_CYBERSE)
+end
+function c511600055.dircon(e)
+	return e:GetHandler():GetMutualLinkedGroup():IsExists(c511600055.dirfilter,1,nil)
+end
+function c511600055.ctcon(e,tp,eg,ep,ev,re,r,rp)
+	local at=Duel.GetAttacker()
+	return at:IsControler(tp) and at:IsType(TYPE_LINK) and at:IsRace(RACE_CYBERSE) and at~=e:GetHandler()
+end
+function c511600055.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,e:GetHandler(),1,0,0x55)
+end
+function c511600055.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		c:AddCounter(0x55,1)
+	end
+end
