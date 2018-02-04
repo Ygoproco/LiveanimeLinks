c430087b5c4e30fb094f8c3e83d838296c3c7486
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2018-02-04 21:59:50 +0800

    Alpha-Wave Emission effect update

---
+++ script/c800000012.lua
@@ -0,0 +1,25 @@
+-- Alpha-Wave Emission
+-- scripted by: UnknownGuest
+--fixed by MLD
+function c800000012.initial_effect(c)
+	-- activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	-- cannot attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	c:RegisterEffect(e2)
+	-- loses 300 ATK
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetValue(-300)
+	c:RegisterEffect(e3)
+end
