74fce8eb4e90a3994885f076e5887e2e3b5ddae7
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2018-02-04 22:33:19 +0800

    Puppet King Anime effect update

--- script/c511002732.lua
+++ script/c511002732.lua
@@ -1,25 +1,28 @@
 --Puppet King
 function c511002732.initial_effect(c)
-	--special summon
+	--summon with 1 tribute
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c511002732.spcon)
-	e1:SetOperation(c511002732.spop)
+	e1:SetDescription(aux.Stringid(3825890,0))
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SUMMON_PROC)
+	e1:SetCondition(c511002732.otcon)
+	e1:SetOperation(c511002732.otop)
+	e1:SetValue(SUMMON_TYPE_ADVANCE)
 	c:RegisterEffect(e1)
 end
-function c511002732.spfilter(c,ft,tp)
-	return c:IsCode(511002731) and (ft>0 or (c:IsControler(tp) and c:GetSequence()<5)) and (c:IsControler(tp) or c:IsFaceup())
+function c511002732.otfilter(c,tp)
+	return c:IsCode(511002731) and (c:IsControler(tp) or c:IsFaceup())
 end
-function c511002732.spcon(e,c)
+function c511002732.otcon(e,c,minc)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	return ft>-1 and Duel.CheckReleaseGroup(tp,c511002732.spfilter,1,nil,ft,tp)
+	local mg=Duel.GetMatchingGroup(c511002732.otfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tp)
+	return c:GetLevel()>6 and minc<=1 and Duel.CheckTribute(c,1,1,mg)
 end
-function c511002732.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(tp,c511002732.spfilter,1,1,nil,Duel.GetLocationCount(tp,LOCATION_MZONE),tp)
-	Duel.Release(g,REASON_COST)
+function c511002732.otop(e,tp,eg,ep,ev,re,r,rp,c)
+	local mg=Duel.GetMatchingGroup(c511002732.otfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tp)
+	local sg=Duel.SelectTribute(tp,c,1,1,mg)
+	c:SetMaterial(sg)
+	Duel.Release(sg,REASON_SUMMON+REASON_MATERIAL)
 end
