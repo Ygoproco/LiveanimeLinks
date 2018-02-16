b76b83ec3e1bed1baa96e0267b65e8c6857569a2
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-02-16 12:00:47 +0100

    Spectre Scripts part 1
    Sunvine Maiden
    Sunavalon Dryas
    Sunavalon Dryades
    Sunvine Shrine
    Sunvine Burial
    Sunavalon Daphne
    Sunvine Healer

---
+++ script/c511009678.lua
@@ -0,0 +1,62 @@
+--Sunavalon Daphne
+function c511009678.initial_effect(c)
+	
+	--link summon
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_PLANT),2)
+	
+	--cannot link material
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+	e1:SetCondition(c511009678.matcon)
+	c:RegisterEffect(e1)
+	
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	c:RegisterEffect(e2)
+	
+	--damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(11384280,0))
+	e3:SetCategory(CATEGORY_DAMAGE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCost(c511009678.cost)
+	e3:SetTarget(c511009678.target)
+	e3:SetOperation(c511009678.operation)
+	c:RegisterEffect(e3)
+end
+function c511009678.matcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsSummonType(SUMMON_TYPE_LINK) and c:GetTurnID()==Duel.GetTurnCount()
+end
+
+function c511009678.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,nil,1,nil) end
+	local sg=Duel.SelectReleaseGroup(tp,nil,1,1,nil)
+	Duel.Release(sg,REASON_COST)
+end
+function c511009678.filter(c)
+	return c:IsType(TYPE_LINK) and c:IsRace(RACE_ZOMBIE) and c:IsAbleToExtra()
+end
+function c511009678.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c511009678.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c511009678.filter,tp,LOCATION_GRAVE,0,2,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,c511009678.filter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,2,0,0)
+end
+function c511009678.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if sg:GetCount()>0 then
+		Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
+	end
+end
