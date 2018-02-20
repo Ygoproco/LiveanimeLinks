0a50cc66b7a258b801319326dbb8200dc6b2d86c
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2018-02-20 15:43:35 -0500

    Fix for Dark Tuner (#128)

--- script/c511010520.lua
+++ script/c511010520.lua
@@ -1,24 +1,35 @@
 --Dark Tuner - Dark Goddess Witaka (Anime)
 --ＤＴ 黒の女神ウィタカ
 function c511010520.initial_effect(c)
-	--special summon
+	--synchro summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c511010520.spcon)
-	e1:SetOperation(c511010520.spop)
-	c:RegisterEffect(e1)
-	--lvchange
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetValue(c511010520.synlimit)
+	c:RegisterEffect(e1)		
+	--special summon
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_LVCHANGE)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetTarget(c511010520.lvtg)
-	e2:SetOperation(c511010520.lvop)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCondition(c511010520.spcon)
+	e2:SetOperation(c511010520.spop)
 	c:RegisterEffect(e2)
+	--lvchange
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_LVCHANGE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetTarget(c511010520.lvtg)
+	e3:SetOperation(c511010520.lvop)
+	c:RegisterEffect(e3)
+end
+function c511010520.synlimit(e,c)
+	if not c then return false end
+	return not c:IsSetCard(0x601)
 end
 function c511010520.spcon(e,c)
 	if c==nil then return true end
