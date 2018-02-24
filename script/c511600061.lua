6995f6871642823da9fa1a16065c776a732706bb
Larry126 <larry126@users.noreply.github.com>
2018-02-24 10:18:37 +0000

    Add files via upload

---
+++ script/c511600061.lua
@@ -0,0 +1,31 @@
+--暗黒魔族ギルファー・デーモン (Anime)
+--Archfiend of Gilfer (Anime)
+--scripted by Larry126
+function c511600061.initial_effect(c)
+	--atk change
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(17979378,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetTarget(c511600061.target)
+	e1:SetOperation(c511600061.operation)
+	c:RegisterEffect(e1)
+end
+function c511600061.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,nil,1,tp,LOCATION_MZONE)
+end
+function c511600061.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-500)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		g:GetFirst():RegisterEffect(e1)
+	end
+end
