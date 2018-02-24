fac00d4b7ce05f194bd8128eea2597fbbaf0e975
Larry126 <larry126@users.noreply.github.com>
2018-02-24 07:27:00 +0000

    removing redundant codes

--- script/c511000467.lua
+++ script/c511000467.lua
@@ -1,6 +1,5 @@
 --砂の砦
 --Sand Fortress
---updated by Larry126
 function c511000467.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
@@ -31,45 +30,9 @@ function c511000467.initial_effect(c)
 	e5:SetCode(EFFECT_ADD_TYPE)
 	e5:SetValue(TYPE_SPELL)
 	c:RegisterEffect(e5)
-	if not c511000467.global_check then
-		c511000467.global_check=true
-		--move
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-		ge1:SetCode(EVENT_SSET)
-		ge1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_IMMEDIATELY_APPLY)
-		ge1:SetOperation(c511000467.setop)
-		Duel.RegisterEffect(ge1,0)
-		--move
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-		ge2:SetCode(EVENT_CHAINING)
-		ge2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_IMMEDIATELY_APPLY)
-		ge2:SetOperation(c511000467.setop)
-		Duel.RegisterEffect(ge2,0)
-	end
 end
 function c511000467.handcon(e)
 	return Duel.GetTurnPlayer()==e:GetHandlerPlayer()
-end
-function c511000467.setop(e,tp,eg,ep,ev,re,r,rp)
-	local g
-	if not eg then g=Group.FromCards(re:GetHandler()):Filter(Card.IsCode,nil,511000467)
-	else g=eg:Filter(Card.IsCode,nil,511000467) end
-	local tc=g:GetFirst()
-	while tc do
-		local fc=Duel.GetFieldCard(1-tp,LOCATION_SZONE,5)
-		if Duel.IsDuelType(DUEL_OBSOLETE_RULING) then
-			if fc then Duel.Destroy(fc,REASON_RULE) end
-			fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
-			if fc and Duel.Destroy(fc,REASON_RULE)==0 then Duel.SendtoGrave(tc,REASON_RULE) end
-		else
-			fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
-			if fc and Duel.SendtoGrave(fc,REASON_RULE)==0 then Duel.SendtoGrave(tc,REASON_RULE) end
-		end
-		Duel.MoveSequence(tc,5)
-		tc=g:GetNext()
-	end
 end
 function c511000467.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetAttackTarget()==nil and ep==tp
