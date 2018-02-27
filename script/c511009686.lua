50abdab7fd0e87bbf0641d6b2dc307b476fd522f
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-02-27 16:07:26 +0100

    Fixed Gouki Shout

--- script/c511009686.lua
+++ script/c511009686.lua
@@ -10,7 +10,7 @@ function c511009686.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetCondition(c511009686.atkcon)
 	e2:SetTarget(c511009686.atktg)
 	e2:SetValue(c511009686.atkval)
@@ -18,18 +18,15 @@ function c511009686.initial_effect(c)
 end
 function c511009686.atkcon(e)
 	c511009686[0]=false
-	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL 
+	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL
 end
 function c511009686.atktg(e,c)
 	return c==Duel.GetAttacker() and c:IsSetCard(0xfc)
 end
-function c511009686.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0xfc)
-end
 function c511009686.atkval(e,c)
 	local sg=Duel.GetMatchingGroup(c511009686.filter,tp,LOCATION_MZONE,0,nil)
 	local atk=sg:GetCount()*300
-	if c511009686[0] then
+	if c511009686[0] or Duel.GetAttacker():IsSetCard(0xfc) then
 		c511009686[0]=true
 		return atk
 	else return 0 end
