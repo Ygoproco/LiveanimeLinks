bd492d50e2d562a8fef47b14c30dcdea87e9b6aa
Larry126 <larry126@users.noreply.github.com>
2018-03-16 18:52:50 +0000

    Various fixes and updates
    Odd-Eyes Phantasma Dragon updates
    Clear Wing Fast Dragon/Odd-Eyes Raging Dragon/Odd-Eyes Rebellion Dragon corrected effect

--- script/c511004423.lua
+++ script/c511004423.lua
@@ -1,37 +1,46 @@
---Odd-Eyes Phantasma Dragon
---cleaned up by MLD
+--オッドアイズ・ファンタズマ・ドラゴン (Anime)
+--Odd-Eyes Phantasma Dragon (Anime)
 function c511004423.initial_effect(c)
+	--pendulum summon
 	aux.EnablePendulumAttribute(c)
+	--overscale
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(511004423)
+	e1:SetCondition(c511004423.spcon)
+	c:RegisterEffect(e1)
+	--attack down
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(511004423)
-	e2:SetCondition(c511004423.spcon)
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetDescription(aux.Stringid(511004423,2))
+	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(c511004423.atkcon)
+	e2:SetOperation(c511004423.atkop)
 	c:RegisterEffect(e2)
-	--attack
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_ATKCHANGE)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e3:SetOperation(c511004423.op)
-	c:RegisterEffect(e3)
 end
 function c511004423.spcon(e)
 	return Duel.IsExistingMatchingCard(Card.IsSetCard,e:GetHandlerPlayer(),LOCATION_EXTRA,0,1,nil,0x99)
 end
-function c511004423.filter(c)
-	return c:IsFaceup() and c:IsType(TYPE_PENDULUM)
+function c511004423.atkfilter(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsFaceup()
 end
-function c511004423.op(e,tp,eg,ev,ep,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
-	local ct=Duel.GetMatchingGroupCount(c511004423.filter,tp,LOCATION_EXTRA,0,nil)*1000
-	local tc=g:GetFirst()
-	while tc do
+function c511004423.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local d=c:GetBattleTarget()
+	local gc=Duel.GetMatchingGroupCount(c511004423.atkfilter,tp,LOCATION_EXTRA,0,nil)
+	return c==Duel.GetAttacker() and d and d:IsFaceup() and not d:IsControler(tp) and gc>0
+end
+function c511004423.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local d=Duel.GetAttacker():GetBattleTarget()
+	local gc=Duel.GetMatchingGroupCount(c511004423.atkfilter,tp,LOCATION_EXTRA,0,nil)
+	if d:IsRelateToBattle() and d:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(-ct)
-		e1:SetReset(RESET_EVENT+0x1fe0000)
-		tc:RegisterEffect(e1)
-		tc=g:GetNext()
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+		e1:SetValue(-gc*1000)
+		d:RegisterEffect(e1)
 	end
 end
