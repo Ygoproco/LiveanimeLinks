860986d505738eec15aac76c4d703e2ee24a2141
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-03-30 16:57:42 -0300

    fix related to union (#171)
    fix related to union

---
+++ script/c511000108.lua
@@ -0,0 +1,15 @@
+--Beetron-2 Beetleturbo
+function c511000108.initial_effect(c)
+	aux.AddUnionProcedure(c,aux.FilterBoolFunction(Card.IsCode,511000107),true,false)
+	--Atk up
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(400)
+	e1:SetCondition(aux.IsUnionState)
+	c:RegisterEffect(e1)
+	--Def up
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e2)
+end
