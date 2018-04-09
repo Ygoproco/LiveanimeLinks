2c8ac8d0cd289a8a57e691e25df30b08de2ec111
GameMaster719 <cobaltss87@yahoo.com>
2018-04-09 11:27:27 -0600

    Update c511005849.lua

--- script/c511005849.lua
+++ script/c511005849.lua
@@ -3,7 +3,6 @@
 function c511005849.initial_effect(c)
 --Activate
 local e1=Effect.CreateEffect(c)
-e1:SetCategory(CATEGORY_POSITION)
 e1:SetType(EFFECT_TYPE_ACTIVATE)
 e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 e1:SetCode(EVENT_FREE_CHAIN)
