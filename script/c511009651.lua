f77a7cc751f03855303e28f8ca294382f0689d52
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2018-04-14 07:10:04 -0400

    Fixed Trickstar Bella Madonna (Anime)

--- script/c511009651.lua
+++ script/c511009651.lua
@@ -1,7 +1,7 @@
 --Trickstars Bella Madonna
 function c511009651.initial_effect(c)
 	--link summon
-	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xfb),2)
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkSetCard,0xfb),2)
 	c:EnableReviveLimit()
 	--immune
 	local e1=Effect.CreateEffect(c)
@@ -10,16 +10,15 @@ function c511009651.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCondition(c511009651.imcon)
-	e1:SetValue(c511009651.efilter)
+	e1:SetValue(c511009651.immval)
 	c:RegisterEffect(e1)
 	--damage
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(101004038,0))
+	e2:SetDescription(aux.Stringid(41302052,0))
 	e2:SetCategory(CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,511009651)
+	e2:SetCountLimit(1,41302052)
 	e2:SetCondition(c511009651.damcon)
 	e2:SetTarget(c511009651.damtg)
 	e2:SetOperation(c511009651.damop)
@@ -30,10 +29,10 @@ function c511009651.imcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsSummonType(SUMMON_TYPE_LINK) and c:GetLinkedGroupCount()==0
 end
-function c511009651.efilter(e,te)
+function c511009651.immval(e,te)
 	return te:GetOwner()~=e:GetOwner()
 end
-function c511009651.damcon(e)
+function c511009651.damcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetLinkedGroupCount()==0
 end
 function c511009651.damfilter(c)
@@ -41,14 +40,16 @@ function c511009651.damfilter(c)
 end
 function c511009651.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c511009651.damfilter,tp,LOCATION_GRAVE,0,1,nil) end
-	local g=Duel.GetMatchingGroupCount(c511009651.damfilter,tp,LOCATION_GRAVE,0,nil)
+	local g=Duel.GetMatchingGroup(c511009651.damfilter,tp,LOCATION_GRAVE,0,nil)
 	local dam=g:GetClassCount(Card.GetCode)*200
 	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(dam)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
 end
 function c511009651.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	local g=Duel.GetMatchingGroupCount(c511009651.damfilter,tp,LOCATION_GRAVE,0,nil)
+	local g=Duel.GetMatchingGroup(c511009651.damfilter,tp,LOCATION_GRAVE,0,nil)
 	local dam=g:GetClassCount(Card.GetCode)*200
 	Duel.Damage(p,dam,REASON_EFFECT)
 end
+
