618634488a4c77ac44a83bc2dc31e619cfe237e2
Larry126 <larry126@users.noreply.github.com>
2018-04-20 00:34:29 +0800

    Fixes
    Typos fixes
    Barrel Dragon Anime update

---
+++ script/c511000769.lua
@@ -0,0 +1,37 @@
+--リボルバー・ドラゴン (Anime)
+--Barrel Dragon (Anime)
+--scripted by Larry126
+function c511000769.initial_effect(c)
+	--attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(c511000769.condition)
+	e1:SetOperation(c511000769.operation)
+	c:RegisterEffect(e1)
+end
+function c511000769.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function c511000769.operation(e,tp,eg,ep,ev,re,r,rp)
+	local t1,t2,t3=Duel.TossCoin(tp,3)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local ct=t1+t2+t3
+	if ct>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EXTRA_ATTACK)
+		e1:SetValue(t1+t2+t3-1)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE)
+		c:RegisterEffect(e1)
+	elseif ct==0 then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_CANNOT_ATTACK)
+		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE)
+		c:RegisterEffect(e2)
+	end
+end
