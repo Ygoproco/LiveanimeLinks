5fec22b368b3f73a368b99ab74c8a9c870046cec
Larry126 <larry126@users.noreply.github.com>
2018-04-26 07:26:25 +0100

    Helixx Necro Darwin fix
    Fix both summon and damage to 0 effects

--- script/c511009617.lua
+++ script/c511009617.lua
@@ -21,46 +21,47 @@ function c511009617.initial_effect(c)
 	e2:SetTarget(c511009617.sptg)
 	e2:SetOperation(c511009617.spop)
 	c:RegisterEffect(e2)
-	--counter
+	--atk change
 	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_COUNTER)
+	e3:SetDescription(aux.Stringid(16617334,0))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_CHAINING)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCondition(aux.damcon1)
-	e3:SetOperation(c511009617.ctop)
+	e3:SetOperation(c511009617.damop)
 	c:RegisterEffect(e3)
 end
 function c511009617.atcon(e)
 	return e:GetHandler():GetMutualLinkedGroupCount()>0
 end
 function c511009617.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
 	local zone=Duel.GetLinkedZone(tp)&0x1f
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and zone~=0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK,tp,zone) end
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function c511009617.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local zone=Duel.GetLinkedZone(tp)&0x1f
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_ATTACK,zone)
+	if c:IsRelateToEffect(e) and zone~=0 then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP,zone)
 	end
 end
-function c511009617.ctop(e,tp,eg,ep,ev,re,r,rp)
+function c511009617.damop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetCode(EFFECT_CHANGE_DAMAGE)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-		e1:SetTargetRange(1,0)
-		e1:SetLabel(cid)
-		e1:SetValue(c511009617.damval)
-		e1:SetReset(RESET_CHAIN)
-		Duel.RegisterEffect(e1,tp)
+	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetLabel(cid)
+	e1:SetValue(c511009617.damval)
+	e1:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(e1,tp)
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
 		Duel.BreakEffect()
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
@@ -69,4 +70,10 @@ function c511009617.ctop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1)
 	end
+end
+function c511009617.damval(e,re,val,r,rp,rc)
+	local cc=Duel.GetCurrentChain()
+	if cc==0 or bit.band(r,REASON_EFFECT)==0 then return val end
+	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
+	if cid==e:GetLabel() then return 0 else return val end
 end
