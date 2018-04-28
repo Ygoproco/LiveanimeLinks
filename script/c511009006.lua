0f9a9b6a230d2a1a83860f6cb28802b95f667a27
Larry126 <larry126@users.noreply.github.com>
2018-04-28 06:42:07 +0100

    Add files via upload

--- script/c511009006.lua
+++ script/c511009006.lua
@@ -1,36 +1,36 @@
 --Mystic Factor Melchior
-function c511009007.initial_effect(c)
-
-	--spsummon
-	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_PZONE)
-	e2:SetCountLimit(1)
-	e2:SetTarget(c511009006.sptg)
-	e2:SetOperation(c511009006.spop)
-	c:RegisterEffect(e2)
+function c511009006.initial_effect(c)
+	--special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(6205579,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(c511009006.sptg)
+	e3:SetOperation(c511009006.spop)
+	c:RegisterEffect(e3)
 end
-
-function c511009006.filter1(c,e)
+function c511009006.spfilter1(c,e)
 	return c:IsOnField() and not c:IsImmuneToEffect(e)
 end
-function c511009006.filter2(c,e,tp,m,f,chkf)
+function c511009006.spfilter2(c,e,tp,m,f,gc,chkf)
 	return c:IsType(TYPE_FUSION) and (not f or f(c))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,gc,chkf)
 end
 function c511009006.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
 	if chk==0 then
 		local chkf=tp
 		local mg1=Duel.GetFusionMaterial(tp):Filter(Card.IsOnField,nil)
-		local res=Duel.IsExistingMatchingCard(c511009006.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
+		local res=Duel.IsExistingMatchingCard(c511009006.spfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,c,chkf)
 		if not res then
 			local ce=Duel.GetChainMaterial(tp)
 			if ce~=nil then
 				local fgroup=ce:GetTarget()
 				local mg2=fgroup(ce,e,tp)
 				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(c511009006.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,chkf)
+				res=Duel.IsExistingMatchingCard(c511009006.spfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,c,chkf)
 			end
 		end
 		return res
@@ -38,10 +38,11 @@ function c511009006.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c511009006.spop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local c=e:GetHandler()
 	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp):Filter(c511009006.filter1,nil,e)
-	local sg1=Duel.GetMatchingGroup(c511009006.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
+	if c:IsFacedown() or not c:IsRelateToEffect(e) or c:IsImmuneToEffect(e) then return end
+	local mg1=Duel.GetFusionMaterial(tp):Filter(c511009006.spfilter1,nil,e)
+	local sg1=Duel.GetMatchingGroup(c511009006.spfilter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,c,chkf)
 	local mg2=nil
 	local sg2=nil
 	local ce=Duel.GetChainMaterial(tp)
@@ -49,7 +50,7 @@ function c511009006.spop(e,tp,eg,ep,ev,re,r,rp)
 		local fgroup=ce:GetTarget()
 		mg2=fgroup(ce,e,tp)
 		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(c511009006.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
+		sg2=Duel.GetMatchingGroup(c511009006.spfilter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,c,chkf)
 	end
 	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
 		local sg=sg1:Clone()
@@ -58,13 +59,13 @@ function c511009006.spop(e,tp,eg,ep,ev,re,r,rp)
 		local tg=sg:Select(tp,1,1,nil)
 		local tc=tg:GetFirst()
 		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
+			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,c,chkf)
 			tc:SetMaterial(mat1)
 			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
 			Duel.BreakEffect()
 			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
 		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
+			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,c,chkf)
 			local fop=ce:GetOperation()
 			fop(ce,e,tp,tc,mat2)
 		end
--- script/c511009007.lua
+++ script/c511009007.lua
@@ -2,9 +2,10 @@
 function c511009007.initial_effect(c)
 	--level up
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(511009007,0))
+	e1:SetDescription(aux.Stringid(44635489,0))
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
 	e1:SetTarget(c511009007.lvtg)
 	e1:SetOperation(c511009007.lvop)
 	c:RegisterEffect(e1)
@@ -14,26 +15,26 @@ function c511009007.lvfilter(c)
 end
 function c511009007.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:IsLevelAbove(3) and Duel.IsExistingMatchingCard(c511009007.lvfilter,tp,LOCATION_MZONE,0,1,c) end
+	if chk==0 then return c:IsFaceup() and c:IsLevelAbove(3)
+		and Duel.GetMatchingGroupCount(c511009007.lvfilter,tp,LOCATION_MZONE,0,c)==Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)-1 end
 end
 function c511009007.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local tg=Duel.GetMatchingGroup(c511009007.lvfilter,tp,LOCATION_MZONE,0,nil)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_LEVEL)
-	e1:SetValue(-2)
-	e1:SetReset(RESET_EVENT+0xfe0000)
-	c:RegisterEffect(e1)
-
-	local tc=tg:GetFirst()
-	while tc do
+	local tg=Duel.GetMatchingGroup(c511009007.lvfilter,tp,LOCATION_MZONE,0,c)
+	if c:IsRelateToEffect(e) and c:IsFaceup() and c:IsLevelAbove(3) and #tg==Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)-1 then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CHANGE_LEVEL_FINAL)
-		e1:SetValue(1)
-		e1:SetReset(RESET_EVENT+0xfe0000)
-		tc:RegisterEffect(e1)
-		tc=tg:GetNext()
-	end		
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(-2)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		c:RegisterEffect(e1)
+		for tc in aux.Next(tg) do
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_CHANGE_LEVEL_FINAL)
+			e2:SetValue(1)
+			e2:SetReset(RESET_EVENT+0x1fe0000)
+			tc:RegisterEffect(e2)
+		end
+	end
 end
