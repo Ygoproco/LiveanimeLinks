b4a8b17247ef9eb9d8871a19eafd5e716d6a7177
Larry126 <larry126@users.noreply.github.com>
2018-05-05 22:44:45 +0100

    Fixes
    Revert Helixx and Dark Mummy
    Fixed spcost for Defcon Bird
    Defcon Bird lore updated

--- script/c511009617.lua
+++ script/c511009617.lua
@@ -1,5 +1,6 @@
+--地獄螺戦鬼ネクロ・ダーウィン
 --Helixx Necro Darwin
---fixed by MLD
+--fixed by Larry126
 function c511009617.initial_effect(c)
 	--link summon
 	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x573),2,2)
@@ -22,33 +23,35 @@ function c511009617.initial_effect(c)
 	e2:SetTarget(c511009617.sptg)
 	e2:SetOperation(c511009617.spop)
 	c:RegisterEffect(e2)
-	--counter
+	--atk change
 	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(16617334,0))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_CHAINING)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCondition(aux.damcon1)
-	e3:SetOperation(c511009617.ctop)
+	e3:SetOperation(c511009617.damop)
 	c:RegisterEffect(e3)
 end
 function c511009617.atcon(e)
 	return e:GetHandler():GetMutualLinkedGroupCount()>0
 end
 function c511009617.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local zone=Duel.GetLinkedZone(tp)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and zone~=0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	local zone=Duel.GetLinkedZone(tp)&0x1f
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function c511009617.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local zone=Duel.GetLinkedZone(tp)
-	if zone>0 and c:IsRelateToEffect(e) then
+	local zone=Duel.GetLinkedZone(tp)&0x1f
+	if c:IsRelateToEffect(e) and zone~=0 then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP,zone)
 	end
 end
-function c511009617.ctop(e,tp,eg,ep,ev,re,r,rp)
+function c511009617.damop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
 	local e1=Effect.CreateEffect(c)
@@ -60,7 +63,7 @@ function c511009617.ctop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetValue(c511009617.damval)
 	e1:SetReset(RESET_CHAIN)
 	Duel.RegisterEffect(e1,tp)
-	if c:IsRelateToEffect(e) then
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
 		Duel.BreakEffect()
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
@@ -72,8 +75,7 @@ function c511009617.ctop(e,tp,eg,ep,ev,re,r,rp)
 end
 function c511009617.damval(e,re,val,r,rp,rc)
 	local cc=Duel.GetCurrentChain()
-	if cc==0 or r&REASON_EFFECT==0 then return end
+	if cc==0 or r&REASON_EFFECT==0 then return val end
 	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
-	if cid==e:GetLabel() then return 0 end
-	return val
+	if cid==e:GetLabel() then return 0 else return val end
 end
