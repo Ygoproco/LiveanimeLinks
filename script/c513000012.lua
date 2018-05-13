baf8620ee09d34b3f0039f7ec7c06afb9055859b
senpaizuri3 <senpaizuri3@gmail.com>
2018-05-12 22:23:53 -0400

    Update c513000012.lua

--- script/c513000012.lua
+++ script/c513000012.lua
@@ -94,6 +94,7 @@ function c513000012.mtop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetReset(RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1)
 	end
 end
