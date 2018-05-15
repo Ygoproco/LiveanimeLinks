14f0ef6defb593580232b58a4c5faccba9db7979
Larry126 <larry126@users.noreply.github.com>
2018-05-15 20:07:32 +0100

    Fixed Give Control Zone Selection

--- script/c95000001.lua
+++ script/c95000001.lua
@@ -1,6 +1,7 @@
+--Darkness/Monster A
 --Darkness Outsider
 function c95000001.initial_effect(c)
-    --no type/attribute/level
+	--no type/attribute/level
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -24,14 +25,14 @@ function c95000001.initial_effect(c)
 end
 c95000001.mark=0
 function c95000001.spfilter(c,e,tp)
-    return c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,true,false) 
+	return c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,true,false) 
 end
 function c95000001.filter(c,ft)
 	return c:IsControlerCanBeChanged() and (ft>0 or c:GetSequence()<5)
 end
 function c95000001.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE,0)
-    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c95000001.filter(chkc,ft) end 
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c95000001.filter(chkc,ft) end 
 	if chk==0 then return ft>-1 and Duel.IsExistingTarget(c95000001.filter,tp,LOCATION_MZONE,0,1,nil,ft) 
 		and Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)>0 and Duel.IsPlayerCanSpecialSummon(tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
@@ -39,13 +40,13 @@ function c95000001.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.GetMatchingGroup(c95000001.spfilter,tp,0,LOCATION_DECK,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,tc,1,0,0)
 	if g:GetCount()>0 then 
-	    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 	end
 end
 function c95000001.spop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
+	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and Duel.GetControl(tc,1-tp) then
-	    local g=Duel.GetMatchingGroup(c95000001.spfilter,tp,0,LOCATION_DECK,nil,e,tp) 
+		local g=Duel.GetMatchingGroup(c95000001.spfilter,tp,0,LOCATION_DECK,nil,e,tp) 
 		if g:GetCount()>0 then
 			Duel.ConfirmCards(tp,g)
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
