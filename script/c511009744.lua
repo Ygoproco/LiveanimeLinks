54e6e22df6d94955044226c01bf633b74bd218a0
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-06-03 01:53:59 +0200

    added Drone General

---
+++ script/c511009744.lua
@@ -0,0 +1,107 @@
+--Battledrone General
+function c511009744.initial_effect(c)
+	--link summon
+	aux.AddLinkProcedure(c,c511009744.mfilter,2)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(511009744,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(c511009744.sptg)
+	e1:SetOperation(c511009744.spop)
+	c:RegisterEffect(e1)
+	
+	-- Damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(511009744,1))
+	e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_BATTLE_DAMAGE)
+	e2:SetCondition(c511009744.damcon)
+	e2:SetCost(c511009744.damcost)
+	e2:SetTarget(c511009744.rectg)
+	e2:SetOperation(c511009744.recop)
+	c:RegisterEffect(e2)
+	--direct atk
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(79185500,2))
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(c511009744.target)
+	e3:SetOperation(c511009744.operation)
+	c:RegisterEffect(e3)
+	aux.CallToken(420)
+end
+function c511009744.mfilter(c,lc,sumtype,tp)
+	return c:IsDrone()
+end
+function c511009744.spfilter(c,e,tp,zone)
+	return c:IsDrone() and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
+end
+function c511009744.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local zone=e:GetHandler():GetLinkedZone(tp)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c511009744.spfilter(chkc,e,tp,zone) end
+	if chk==0 then return Duel.IsExistingTarget(c511009744.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,zone) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c511009744.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,zone)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c511009744.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local zone=c:GetLinkedZone(tp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP,zone)~=0
+	end
+end
+
+function c511009744.damcon(e,tp,eg,ep,ev,re,r,rp)
+	if ep==tp then return false end
+	local rc=eg:GetFirst()
+	return rc:IsControler(tp) and rc:IsDrone() and Duel.GetAttackTarget()==nil
+end
+function c511009744.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local rc=eg:GetFirst()
+	if chk==0 then return ep==tp and rc and rc:IsReleasable() end
+	e:SetLabel(rc:GetAttack())
+	Duel.Release(rc,REASON_COST)
+end
+
+function c511009744.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(e:GetLabel())
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,ev)
+end
+function c511009744.damop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
+
+
+function c511009744.filter(c)
+	return c:IsFaceup() and c:IsDrone() and c:IsLevelBelow(4) and c:IsAttackBelow(1000) and not c:IsHasEffect(EFFECT_DIRECT_ATTACK)
+end
+function c511009744.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c511009744.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c511009744.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c511009744.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c511009744.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DIRECT_ATTACK)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
