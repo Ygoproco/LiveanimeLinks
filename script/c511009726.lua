6b2fd0cfc0cd13246e3ea807b0f0f2df2fc8a0d1
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2018-07-13 17:05:41 -0400

    Fixed Link Fire's Return (#257)

--- script/c511009726.lua
+++ script/c511009726.lua
@@ -11,7 +11,7 @@ function c511009726.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c511009726.filter(c,e,tp)
-	return c:IsType(TYPE_LINK) and c:IsSetCard(0x578) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+	return c:IsType(TYPE_LINK) and c:IsSetCard(0x578) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
 function c511009726.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c511009726.filter(chkc,e,tp) end
@@ -24,7 +24,7 @@ end
 function c511009726.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)~=0 then
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CANNOT_ATTACK)
