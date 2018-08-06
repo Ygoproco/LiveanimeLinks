d9a14af8ef27845635c4ca2a3ee8fbadb5fcda29
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2018-08-06 08:49:07 -0400

    Fusion of Fire fix (#268)
    Fusion of Fire fix

--- script/c511600181.lua
+++ script/c511600181.lua
@@ -1,6 +1,7 @@
 --フュージョン・オブ・ファイア
 --Fusion of Fire
 --scripted by Larry126
+--rescripted by TOP
 function c511600181.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -11,26 +12,33 @@ function c511600181.initial_effect(c)
 	e1:SetOperation(c511600181.activate)
 	c:RegisterEffect(e1)
 end
-function c511600181.ffilter(c,e,tp,m,f)
-	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x119) and (not f or f(c))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,tp)
+function c511600181.filter0(c)
+	return c:IsFaceup() and c:IsCanBeFusionMaterial()
 end
-function c511600181.filter(c,e)
-	return c:IsOnField() and not c:IsImmuneToEffect(e)
+function c511600181.filter1(c,e)
+	return c:IsFaceup() and c:IsCanBeFusionMaterial() and not c:IsImmuneToEffect(e)
+end
+function c511600181.filter2(c,e,tp,m,f,chkf)
+	return c:IsSetCard(0x119) and (not f or f(c))
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
+end
+function c511600181.filter3(c,e)
+	return not c:IsImmuneToEffect(e)
 end
 function c511600181.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
+		local chkf=tp
 		local mg1=Duel.GetFusionMaterial(tp)
-		local mg2=Duel.GetFusionMaterial(1-tp):Filter(Card.IsOnField,nil)
+		local mg2=Duel.GetMatchingGroup(c511600181.filter0,tp,0,LOCATION_MZONE,nil)
 		mg1:Merge(mg2)
-		local res=Duel.IsExistingMatchingCard(c511600181.ffilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil)
+		local res=Duel.IsExistingMatchingCard(c511600181.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
 		if not res then
 			local ce=Duel.GetChainMaterial(tp)
 			if ce~=nil then
 				local fgroup=ce:GetTarget()
 				local mg3=fgroup(ce,e,tp)
 				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(c511600181.ffilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg3,mf)
+				res=Duel.IsExistingMatchingCard(c511600181.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg3,mf,chkf)
 			end
 		end
 		return res
@@ -38,10 +46,11 @@ function c511600181.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c511600181.activate(e,tp,eg,ep,ev,re,r,rp)
-	local mg1=Duel.GetFusionMaterial(tp):Filter(aux.NOT(Card.IsImmuneToEffect),nil,e)
-	local mg2=Duel.GetFusionMaterial(1-tp):Filter(c511600181.filter,nil,e)
+	local chkf=tp
+	local mg1=Duel.GetFusionMaterial(tp):Filter(c511600181.filter3,nil,e)
+	local mg2=Duel.GetMatchingGroup(c511600181.filter1,tp,0,LOCATION_MZONE,nil,e)
 	mg1:Merge(mg2)
-	local sg1=Duel.GetMatchingGroup(c511600181.ffilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil)
+	local sg1=Duel.GetMatchingGroup(c511600181.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
 	local mg3=nil
 	local sg2=nil
 	local ce=Duel.GetChainMaterial(tp)
@@ -49,7 +58,7 @@ function c511600181.activate(e,tp,eg,ep,ev,re,r,rp)
 		local fgroup=ce:GetTarget()
 		mg3=fgroup(ce,e,tp)
 		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(c511600181.ffilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg3,mf)
+		sg2=Duel.GetMatchingGroup(c511600181.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg3,mf,chkf)
 	end
 	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
 		local sg=sg1:Clone()
@@ -58,13 +67,13 @@ function c511600181.activate(e,tp,eg,ep,ev,re,r,rp)
 		local tg=sg:Select(tp,1,1,nil)
 		local tc=tg:GetFirst()
 		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,tp)
+			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
 			tc:SetMaterial(mat1)
 			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
 			Duel.BreakEffect()
 			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
 		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg3,nil,tp)
+			local mat2=Duel.SelectFusionMaterial(tp,tc,mg3,nil,chkf)
 			local fop=ce:GetOperation()
 			fop(ce,e,tp,tc,mat2)
 		end
