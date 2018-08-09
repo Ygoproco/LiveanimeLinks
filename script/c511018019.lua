9c0a512dbadf6f6e81b06093a2bcc4f01fcfd4af
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-08-08 23:17:02 -0300

    Add files via upload

---
+++ script/c511018019.lua
@@ -0,0 +1,24 @@
+--Winged Kuriboh
+function c511018019.initial_effect(c)
+	--no battle damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetOperation(c511018019.op)
+	c:RegisterEffect(e2)	
+end
+function c511018019.op(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
