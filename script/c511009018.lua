85a4aeb7ea7b417617bf75a6c0999738965da284
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-08-14 00:09:43 -0300

    fix for Gouki The Solid Ogre
    move effect

--- script/c511009018.lua
+++ script/c511009018.lua
@@ -32,19 +32,25 @@ function c511009018.incon(e)
 	return e:GetHandler():GetLinkedGroupCount()>0 
 	and e:GetHandler():GetLinkedGroup():IsExists(c511009018.indesfil,1,nil)
 end
-function c511009018.seqfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xfc) and c:GetSequence()<5
+function c511009018.seqfilter(c,zone)
+	return c:IsFaceup() and c:IsSetCard(0xfc) and c:GetSequence()<5 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE,c:GetControler(),LOCATION_REASON_CONTROL,zone)>0
 end
 function c511009018.seqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c511009018.seqfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c511009018.seqfilter,tp,LOCATION_MZONE,0,1,nil)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	local c=e:GetHandler()
+	local zone=c:GetLinkedZone(tp)&0x1f
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c511009018.seqfilter(chkc,zone) end
+	if chk==0 then return Duel.IsExistingTarget(c511009018.seqfilter,tp,LOCATION_MZONE,0,1,nil,zone) end
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(92204263,1))
-	Duel.SelectTarget(tp,c511009018.seqfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,c511009018.seqfilter,tp,LOCATION_MZONE,0,1,1,nil,zone)
 end
 function c511009018.seqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local zone=c:GetLinkedZone(tp)&0x1f
 	local tc=Duel.GetFirstTarget()
-	if not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) or Duel.GetLocationCount(tc:GetControler(),LOCATION_MZONE,tc:GetControler(),LOCATION_REASON_CONTROL,zone)<=0 then return end
+	local i=0
+	if not c:IsControler(tp) then i=16 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
-	Duel.MoveSequence(tc,math.log(Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0),2))
+	local nseq=math.log(Duel.SelectDisableField(tp,1,LOCATION_MZONE,LOCATION_MZONE,~(zone<<i)),2)-i
+	Duel.MoveSequence(tc,nseq)
 end
