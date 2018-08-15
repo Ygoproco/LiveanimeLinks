7b1b40faa9bab0bc1f2e7db238b5ff0157694bf0
Larry126 <larry126@users.noreply.github.com>
2018-08-15 14:56:35 +0800

    Vrains and fixes

--- script/c511600170.lua
+++ script/c511600170.lua
@@ -1,6 +1,8 @@
---thousand knifes (manga)
+--千本ナイフ (Manga)
+--Thousand Knives (Manga)
+--scripted by Larry126
 function c511600170.initial_effect(c)
-	--activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -10,18 +12,18 @@ function c511600170.initial_effect(c)
 	e1:SetOperation(c511600170.activate)
 	c:RegisterEffect(e1)
 end
-function c511600170.filter(c)
+function c511600170.cfilter(c)
 	return c:IsFaceup() and c:IsRace(RACE_SPELLCASTER)
 end
-function c511600170.condition(e,tp)
-	return Duel.IsExistingMatchingCard(c511600170.filter,tp,LOCATION_MZONE,0,1,nil)
+function c511600170.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c511600170.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function c511600170.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
-	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,sg:GetCount(),0,0)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end
 function c511600170.activate(e,tp,eg,ep,ev,re,r,rp)
-	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
-	Duel.Destroy(sg,REASON_EFFECT)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	Duel.Destroy(g,REASON_EFFECT)
 end
