78781bc663a4a539c32cb918a86acee9dd70d1c6
Larry126 <larry126@users.noreply.github.com>
2018-08-22 02:35:13 +0800

    Ninja fixes

--- script/c511001363.lua
+++ script/c511001363.lua
@@ -45,19 +45,19 @@ function c511001363.initial_effect(c)
 	end
 end
 function c511001363.cfilter(c)
-	return c:IsHasEffect(511002571) and not c:IsHasEffect(5110013630)
+	return c:IsHasEffect(511002571) and #{c:GetCardEffect(5110013630)}==0
 end
 function c511001363.op(e)
 	local g=Duel.GetMatchingGroup(c511001363.cfilter,0,0xff,0xff,nil)
 	for c in aux.Next(g) do
 		local effs={c:GetCardEffect(511002571)}
-		Debug.Message(#effs)
 		for _,eff in ipairs(effs) do
 			local resetflag,resetcount=eff:GetLabelObject():GetReset()
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE,EFFECT_FLAG2_MAJESTIC_MUST_COPY)
+			local prop1,prop2=eff:GetProperty()
+			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_XMATERIAL)
 			e1:SetCode(511001363)
+			e1:SetProperty(prop1,prop2)
 			e1:SetLabel(c:GetOriginalCode())
 			e1:SetLabelObject(eff:GetLabelObject())
 			if resetflag and resetcount then
@@ -66,10 +66,10 @@ function c511001363.op(e)
 				e1:SetReset(resetflag)
 			end
 			c:RegisterEffect(e1,true)
-			local e2=Effect.CreateEffect(e:GetHandler())
+			local e2=Effect.CreateEffect(c)
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetCode(5110013630)
-			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE,EFFECT_FLAG2_MAJESTIC_MUST_COPY)
+			e2:SetProperty(prop1,prop2)
 			if resetflag and resetcount then
 				e2:SetReset(resetflag,resetcount)
 			elseif resetflag then
@@ -120,7 +120,7 @@ function c511001363.filter(c,e,tp)
 	for _,teh in ipairs(effs) do
 		local te=teh:GetLabelObject()
 		local tg=te:GetTarget()
-		if te:GetHandler()==c and te:GetValue()~=1 and (not tg or tg(e,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0)) then return true end
+		if te:GetHandler()==c and te:GetHandler():GetFlagEffect(511001363)==0 and (not tg or tg(e,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0)) then return true end
 	end
 	return false
 end
@@ -132,10 +132,11 @@ function c511001363.copytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	if chk==0 then return (a or b) and #{c:GetCardEffect(511001363)}>0 and ov:IsExists(c511001363.filter,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=ov:FilterSelect(tp,c511001363.filter,1,1,nil,e,tp)
-	Duel.Hint(HINT_CARD,0,g:GetFirst():GetOriginalCode())
+	local tc=ov:FilterSelect(tp,c511001363.filter,1,1,nil,e,tp):GetFirst()
+	tc:RegisterFlagEffect(511001363,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	Duel.Hint(HINT_CARD,0,tc:GetOriginalCode())
 	e:SetProperty(0)
-	Duel.SetTargetCard(g)
+	Duel.SetTargetCard(tc)
 	e:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	local op=0
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(76922029,0))
@@ -149,7 +150,7 @@ function c511001363.copytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		op=1
 	end
 	if op==0 then
-		Duel.SendtoGrave(g,REASON_COST) 
+		Duel.SendtoGrave(tc,REASON_COST) 
 	else
 		Duel.PayLPCost(tp,400)
 	end
@@ -170,7 +171,7 @@ function c511001363.copyop(e,tp,eg,ep,ev,re,r,rp)
 		for _,teh in ipairs(eff) do
 			local temp=teh:GetLabelObject()
 			local tg=temp:GetTarget()
-			if temp:GetValue()~=1 and (not tg or tg(e,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0)) then
+			if temp:GetHandler():GetFlagEffect(511001363)==0 and (not tg or tg(e,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0)) then
 				table.insert(ac,teh)
 				table.insert(acd,temp:GetDescription())
 			end
@@ -207,18 +208,5 @@ function c511001363.copyop(e,tp,eg,ep,ev,re,r,rp)
 				etc=g:GetNext()
 			end
 		end
-		te:SetValue(1)
-		local e3=Effect.CreateEffect(e:GetHandler())
-		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e3:SetCode(EVENT_PHASE+PHASE_END)
-		e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e3:SetCountLimit(1)
-		e3:SetLabelObject(te)
-		e3:SetOperation(c511001363.resetop)
-		e3:SetReset(RESET_PHASE+PHASE_END)
-		Duel.RegisterEffect(e3,tp)
 	end
-end
-function c511001363.resetop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetLabelObject():SetValue(0)
 end
