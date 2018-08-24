2bc93af6e7c4585f133a97edf35d2657363c3d72
Larry126 <larry126@users.noreply.github.com>
2018-08-24 23:19:27 +0800

    Over 100 Chaos Numbers

---
+++ script/c513000059.lua
@@ -0,0 +1,144 @@
+--CNo.101 S・H・Dark Knight (Anime)
+--Number C101: Silent Honor DARK (Anime)
+--fixed by Larry126
+function c513000059.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,5,3)
+	c:EnableReviveLimit()
+	--Rank Up Check
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_MATERIAL_CHECK)
+	e0:SetValue(c513000059.valcheck)
+	c:RegisterEffect(e0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetLabelObject(e0)
+	e1:SetOperation(c513000059.rankupregop)
+	c:RegisterEffect(e1) 
+	--material
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(12744567,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c513000059.target)
+	e2:SetOperation(c513000059.operation)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(12744567,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_RECOVER)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCondition(c513000059.spcon)
+	e3:SetTarget(c513000059.sptg)
+	e3:SetOperation(c513000059.spop)
+	c:RegisterEffect(e3)
+	--battle indestructable
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e4:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,0x48)))
+	c:RegisterEffect(e4)
+	--spsummon2
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetDescription(aux.Stringid(102380,0))
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetLabelObject(e1)
+	e5:SetCost(c513000059.spcost2)
+	e5:SetTarget(c513000059.sptg2)
+	e5:SetOperation(c513000059.spop2)
+	c:RegisterEffect(e5,false,1)
+	aux.CallToken(12744567)
+end
+c513000059.xyz_number=101
+c513000059.listed_names={48739166}
+function c513000059.filter(c)
+	return not c:IsType(TYPE_TOKEN) and c:IsAbleToChangeControler()
+end
+function c513000059.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsType(TYPE_XYZ) 
+		and Duel.IsExistingMatchingCard(c513000059.filter,tp,0,LOCATION_MZONE,1,nil) end
+end
+function c513000059.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPPO)
+	local tc=Duel.SelectMatchingCard(tp,c513000059.filter,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+	if tc and not tc:IsImmuneToEffect(e) then
+		local og=tc:GetOverlayGroup()
+		if og:GetCount()>0 then
+			Duel.SendtoGrave(og,REASON_RULE)
+		end
+		Duel.Overlay(c,Group.FromCards(tc))
+	end
+end
+function c513000059.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetOverlayCount()>0
+end
+function c513000059.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	local rec=e:GetHandler():GetTextAttack()
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(rec)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,rec)
+end
+function c513000059.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+		Duel.Recover(p,d,REASON_EFFECT)
+	end
+end
+function c513000059.rumfilter(c)
+	return c:IsCode(48739166) and not c:IsPreviousLocation(LOCATION_OVERLAY)
+end
+function c513000059.valcheck(e,c)
+	local mg=c:GetMaterial()
+	if mg:IsExists(c513000059.rumfilter,1,nil) then
+		e:SetLabel(1)
+	else
+		e:SetLabel(0)
+	end
+end
+function c513000059.rankupregop(e,tp,eg,ep,ev,re,r,rp)
+	local rc=re:GetHandler()
+	if e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) and (rc:IsSetCard(0x95)
+		or rc:IsCode(100000581) or rc:IsCode(111011002) or rc:IsCode(511000580)
+		or rc:IsCode(511002068) or rc:IsCode(511002164) or rc:IsCode(93238626))
+		and e:GetLabelObject():GetLabel()==1 then
+		e:SetLabel(1)
+	else
+		e:SetLabel(0)
+	end
+end
+function c513000059.spcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_COST)
+		and e:GetLabelObject():GetLabel()==1 end
+	local g=c:GetOverlayGroup()
+	Duel.SendtoGrave(g,REASON_COST) 
+end
+function c513000059.spfilter(c,e,tp)
+	return c:IsCode(48739166) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c513000059.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c513000059.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function c513000059.spop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c513000059.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
