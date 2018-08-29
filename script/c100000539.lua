4231fc12a419b18a09e17f642c389b4e5f298d49
Larry126 <larry126@users.noreply.github.com>
2018-08-29 14:10:52 +0800

    Update c100000539.lua

--- script/c100000539.lua
+++ script/c100000539.lua
@@ -1,4 +1,5 @@
 --英雄の掛橋－ビヴロスト
+--Rainbow Bridge Bifrost
 function c100000539.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -11,7 +12,7 @@ function c100000539.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c100000539.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0x228)
+	return c:IsFaceup() and c:IsSetCard(0x122)
 end
 function c100000539.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100000539.filter(chkc) end
