c24d7ca42cc4d5b64c130941139791520431461e
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-09-04 15:33:25 +0200

    Id fix

--- script/c511009303.lua
+++ script/c511009303.lua
@@ -1,8 +1,8 @@
---G Golem Invalid Dolmen
-function c511009429.initial_effect(c)
+--Metal Baboon, Ruffian of the Forest
+function c511009303.initial_effect(c)
 	c:EnableReviveLimit()
-	aux.AddLinkProcedure(c,c60292055.matfilter,2,2)
+	aux.AddLinkProcedure(c,c511009303.matfilter,2,2)
 end
-function c60292055.matfilter(c,lc,sumtype,tp)
+function c511009303.matfilter(c,lc,sumtype,tp)
 	return c:IsLevelAbove(5) and c:IsRace(RACE_BEAST,lc,sumtype,tp)
 end
