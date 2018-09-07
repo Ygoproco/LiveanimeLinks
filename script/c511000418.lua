30632592bb6e7ff9a1971e811658e3e94b4e012f
Larry126 <larry126@users.noreply.github.com>
2018-09-07 15:45:27 +0100

    Update c511000418.lua

--- script/c511000418.lua
+++ script/c511000418.lua
@@ -10,8 +10,7 @@ function c511000418.initial_effect(c)
 	e1:SetOperation(c511000418.activate)
 	c:RegisterEffect(e1)
 end
-function c511000418.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) end
+function c511000418.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,LOCATION_MZONE)
