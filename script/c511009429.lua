0fe5daeae3f374c9e55efe55e5ce423d892bacc1
Larry126 <larry126@users.noreply.github.com>
2018-09-07 13:44:53 +0100

    Invalid fix

--- script/c511009429.lua
+++ script/c511009429.lua
@@ -1,3 +1,4 @@
+--Ｇゴーレム・インヴァリッド・ドルメン
 --G Golem Invalid Dolmen
 function c511009429.initial_effect(c)
 	c:EnableReviveLimit()
@@ -18,28 +19,35 @@ function c511009429.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(0,LOCATION_MZONE)
 	c:RegisterEffect(e2)
+	local e5=e2:Clone()
+	e5:SetCode(EFFECT_MUST_ATTACK_MONSTER)
+	e5:SetValue(c511009429.atklimit)
+	c:RegisterEffect(e5)
 	--pierce
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_PIERCE)
 	c:RegisterEffect(e3)
-	--spsummon
+	--negate
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(66818682,1))
+	e4:SetDescription(aux.Stringid(86848580,0))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e4:SetCode(EVENT_DESTROYED)
 	e4:SetCondition(c511009429.discon)
 	e4:SetTarget(c511009429.distg)
 	e4:SetOperation(c511009429.disop)
 	c:RegisterEffect(e4)
 end
+function c511009429.atklimit(e,c)
+    return c==e:GetHandler()
+end
 function c511009429.indtg(e,c)
 	return c:GetMutualLinkedGroupCount()>0
 end
 function c511009429.efilter(e,te)
-	return te:GetOwnerPlayer()~=e:GetHandlerPlayer() and te:IsActiveType(TYPE_MONSTER)
+	return e:GetOwnerPlayer()~=te:GetOwnerPlayer() and te:IsActiveType(TYPE_MONSTER) and te:GetHandler():IsOnField()
 end
 function c511009429.discon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
@@ -48,28 +56,25 @@ function c511009429.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.disfilter1,tp,0,LOCATION_ONFIELD,1,nil) end
 end
 function c511009429.disop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(aux.disfilter1,tp,0,LOCATION_ONFIELD,nil)
-	local tc=g:GetFirst()
-	if not tc then return end
 	local c=e:GetHandler()
-	while tc do
+	local g=Duel.GetMatchingGroup(aux.disfilter1,tp,0,LOCATION_ONFIELD,nil)
+	for tc in aux.Next(g) do
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_STANDBY,2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_STANDBY,2)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e2)
 		if tc:IsType(TYPE_TRAPMONSTER) then
 			local e3=Effect.CreateEffect(c)
 			e3:SetType(EFFECT_TYPE_SINGLE)
 			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
-			e3:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_STANDBY,2)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
 			tc:RegisterEffect(e3)
 		end
-		tc=g:GetNext()
 	end
 end
