d0ccb8c6471d50eb47e3ea84aad008dd21f90552
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2018-09-10 10:28:57 -0400

    Added new cards
    Mach Synchron (Anime)
    Variety Comes Out (Anime)

--- script/c511010536.lua
+++ script/c511010536.lua
@@ -1,1 +1,33 @@
-
+--Mach Synchron (Anime)
+function c511010536.initial_effect(c)
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(100000348,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_BE_MATERIAL)
+	e1:SetCondition(c511010536.thcon)
+	e1:SetTarget(c511010536.thtg)
+	e1:SetOperation(c511010536.thop)
+	c:RegisterEffect(e1)
+end
+function c511010536.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_SYNCHRO
+end
+function c511010536.filter(c)
+	return bit.band(c:GetReason(),REASON_SYNCHRO)==REASON_SYNCHRO and c:IsAbleToHand()
+end
+function c511010536.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c511010536.filter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local sg=Duel.SelectTarget(tp,c511010536.filter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,sg,sg:GetCount(),0,0)
+end
+function c511010536.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
