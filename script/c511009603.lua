0d5f7f20c0aa1fa94a3b7cc6b79112267399a08d
pyrQ <30827955+pyrQ@users.noreply.github.com>
2018-10-06 13:17:17 +0300

    "Tentacluster Blastsucker" fix (#351)
    "Tentacluster Blastsucker" fix

--- script/c511009603.lua
+++ script/c511009603.lua
@@ -1,19 +1,17 @@
 --Tentacluster Blastsucker
 function c511009603.initial_effect(c)
-	--destroy
+	--destroy & damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(114932,0)) 
-	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_DESTROY)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,511009603)
 	e1:SetCondition(c511009603.descon)
 	e1:SetTarget(c511009603.destg)
 	e1:SetOperation(c511009603.desop)
 	c:RegisterEffect(e1)
-	
-		--destroy
+	--gy damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(17775525,1))
 	e2:SetCategory(CATEGORY_DAMAGE)
@@ -24,7 +22,6 @@ function c511009603.initial_effect(c)
 	e2:SetTarget(c511009603.damtg)
 	e2:SetOperation(c511009603.damop)
 	c:RegisterEffect(e2)
-	
 end
 function c511009603.descon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -39,7 +36,7 @@ function c511009603.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return c:IsDestructable() and g:GetCount()>0 end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,c,1,0,0)
 	Duel.SetTargetPlayer(1-tp)
-	Duel.SetTargetParam(500)
+	Duel.SetTargetParam(400)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,400)
 end
 function c511009603.desop(e,tp,eg,ep,ev,re,r,rp)
@@ -48,12 +45,11 @@ function c511009603.desop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Damage(p,d,REASON_EFFECT)
 	end
 end
-
 function c511009603.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
-	Duel.SetTargetParam(500)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
+	Duel.SetTargetParam(400)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,400)
 end
 function c511009603.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
