d5fb1ba9aa4051cae04349f0ce67b6853264083a
Larry126 <larry126@users.noreply.github.com>
2018-11-03 06:48:52 +0000

    Rose Wipe

---
+++ script/c511000689.lua
@@ -0,0 +1,16 @@
+--薔薇の鞭
+--Rose Whip
+--fixed by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsCode,76812113))
+	--atk/def up
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(300)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e2)
+end
