89e2d49c2a27fe116e89528e56d939e08f9a52f1
Larry126 <larry126@users.noreply.github.com>
2018-11-24 18:36:51 +0000

    Add files via upload

--- script/c511000800.lua
+++ script/c511000800.lua
@@ -1,5 +1,7 @@
+--天輪鐘楼
 --Celestial Bell Tower
-function c511000800.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -7,30 +9,28 @@ function c511000800.initial_effect(c)
 	c:RegisterEffect(e1)
 	--draw
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(511000800,0))
+	e2:SetDescription(aux.Stringid(90953320,0))
 	e2:SetCategory(CATEGORY_DRAW)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_BOTH_SIDE)
+	e2:SetProperty(EFFECT_FLAG_BOTH_SIDE+EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCondition(c511000800.condition)
-	e2:SetTarget(c511000800.target)
-	e2:SetOperation(c511000800.operation)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
 end
-function c511000800.filter(c,tp)
-	return c:IsControler(tp) and c:GetSummonType()==SUMMON_TYPE_SYNCHRO
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsSummonType,1,nil,SUMMON_TYPE_SYNCHRO) and tp==eg:GetFirst():GetSummonPlayer()
 end
-function c511000800.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c511000800.filter,1,nil,tp) and ep==tp
-end
-function c511000800.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-	Duel.SetTargetPlayer(tp)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local p=eg:GetFirst():GetSummonPlayer()
+	if chk==0 then return Duel.IsPlayerCanDraw(p,1) and not e:GetHandler():IsStatus(STATUS_CHAINING) end
+	Duel.SetTargetPlayer(p)
 	Duel.SetTargetParam(1)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,p,1)
 end
-function c511000800.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
