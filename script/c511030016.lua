6c81130316260b3fc37c14845c72caf0b7891c7b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2018-12-27 01:28:38 +0200

    Add files via upload

--- script/c511030016.lua
+++ script/c511030016.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--negate attack
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0)) --wind up knight 80538728
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BE_BATTLE_TARGET)
