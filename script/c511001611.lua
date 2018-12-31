6df5085a0f28a4b0b258bff3318e844172cdf186
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-12-31 11:15:28 -0200

    small fix for number evaille
    failsafe

--- script/c511001611.lua
+++ script/c511001611.lua
@@ -18,10 +18,13 @@ function c511001611.filter(c,mg)
 		return (mg-c):CheckWithSumEqual(function(c)return c.xyz_number end,c.xyz_number,1,99999)
 	end
 end
+function c511001611.mfilter(c)
+	return c.xyz_number and type(c.xyz_number) == "number"
+end
 function c511001611.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_XYZ)
-		local mg=Duel.GetMatchingGroup(function(c) return c.xyz_number end,tp,LOCATION_EXTRA,0,nil)
+		local mg=Duel.GetMatchingGroup(c511001611.mfilter,tp,LOCATION_EXTRA,0,nil)
 		if #(pg-mg)>0 then return false end
 		return Duel.IsExistingMatchingCard(c511001611.filter,tp,LOCATION_EXTRA,0,1,nil,mg)
 	end
@@ -33,7 +36,7 @@ function c511001611.checkvalid(c,mg,sg,mg0,cc)
 end
 function c511001611.activate(e,tp,eg,ep,ev,re,r,rp)
 	local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_XYZ)
-	local mg,mg0=Duel.GetMatchingGroup(function(c) return c.xyz_number end,tp,LOCATION_EXTRA,0,nil):Split(function(c)return c.xyz_number>0 end,nil)
+	local mg,mg0=Duel.GetMatchingGroup(c511001611.mfilter,tp,LOCATION_EXTRA,0,nil):Split(function(c)return c.xyz_number>0 end,nil)
 	if #(pg-(mg+mg0))>0 then return false end
 	local mat=Group.CreateGroup()
 	while true do
