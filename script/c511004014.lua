08d731b69cede3d2c0070917cc43405d705f23c7
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-02-24 00:07:00 +0200

    Updated "Battle City" with new ID handling

--- script/c511004014.lua
+++ script/c511004014.lua
@@ -1,7 +1,8 @@
 --Battle City
 --Scripted by Edo9300
 --Updated by Larry126
-function c511004014.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c) 
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -9,7 +10,7 @@ function c511004014.initial_effect(c)
 	e1:SetCountLimit(1)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_NO_TURN_RESET+EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetRange(0xff)
-	e1:SetOperation(c511004014.op)
+	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
 	local proc=Duel.SendtoGrave
 	Duel.SendtoGrave=function(tg,r,tp)
@@ -24,33 +25,33 @@ function c511004014.initial_effect(c)
 		return proc(tg,r,tp)
 	end
 end
-function c511004014.op(e,tp,eg,ep,ev,re,r,rp)
+function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.GetFlagEffect(tp,511004014)==0 and Duel.GetFlagEffect(1-tp,511004014)==0 then
+	if Duel.GetFlagEffect(tp,id)==0 and Duel.GetFlagEffect(1-tp,id)==0 then
 		Duel.ConfirmCards(tp,c)
 		Duel.ConfirmCards(1-tp,c)
-		Duel.RegisterFlagEffect(tp,511004014,0,0,0)
+		Duel.RegisterFlagEffect(tp,id,0,0,0)
 		--To controler's grave
 		local e1=Effect.GlobalEffect()
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE) 
 		e1:SetCode(EFFECT_SEND_REPLACE)
-		e1:SetTarget(c511004014.reptg)
-		e1:SetValue(c511004014.repval)
+		e1:SetTarget(s.reptg)
+		e1:SetValue(s.repval)
 		Duel.RegisterEffect(e1,tp)
 		local e1a=Effect.GlobalEffect()
 		e1a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1a:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 		e1a:SetCode(EVENT_ADJUST)
-		e1a:SetCondition(c511004014.grepcon)
-		e1a:SetOperation(c511004014.grepop)
+		e1a:SetCondition(s.grepcon)
+		e1a:SetOperation(s.grepop)
 		Duel.RegisterEffect(e1a,tp)
 		local e1b=Effect.GlobalEffect()
 		e1b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1b:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 		e1b:SetCode(EVENT_CHAIN_END)
-		e1b:SetCondition(c511004014.grepcon2)
-		e1b:SetOperation(c511004014.grepop2)
+		e1b:SetCondition(s.grepcon2)
+		e1b:SetOperation(s.grepop2)
 		Duel.RegisterEffect(e1b,tp)
 		--Double Tribute
 		local e2=Effect.GlobalEffect()
@@ -84,8 +85,8 @@ function c511004014.op(e,tp,eg,ep,ev,re,r,rp)
 		e4:SetCode(EFFECT_LIMIT_SUMMON_PROC)
 		e4:SetTargetRange(0xff,0xff)
 		e4:SetTarget(function(e,c)return c:GetLevel()>=10 and c:GetFlagEffect(511004015)==1 end)
-		e4:SetCondition(c511004014.ttcon)
-		e4:SetOperation(c511004014.ttop)
+		e4:SetCondition(s.ttcon)
+		e4:SetOperation(s.ttop)
 		e4:SetValue(SUMMON_TYPE_ADVANCE)
 		Duel.RegisterEffect(e4,tp)
 		local e5=e4:Clone()
@@ -120,18 +121,18 @@ function c511004014.op(e,tp,eg,ep,ev,re,r,rp)
 		e8:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e8:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE) 
 		e8:SetCode(EVENT_CHAIN_SOLVING)
-		e8:SetOperation(c511004014.negop)
+		e8:SetOperation(s.negop)
 		Duel.RegisterEffect(e8,tp)
 		local e9=e8:Clone()
 		e9:SetCode(EVENT_DESTROYED)
-		e9:SetOperation(c511004014.negop2)
+		e9:SetOperation(s.negop2)
 		Duel.RegisterEffect(e9,tp)
 		--block
 		local e10=Effect.GlobalEffect()
 		e10:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e10:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 		e10:SetCode(EVENT_SUMMON_SUCCESS)
-		e10:SetOperation(c511004014.block)
+		e10:SetOperation(s.block)
 		Duel.RegisterEffect(e10,tp)
 		local e10a=e10:Clone()
 		e10a:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
@@ -151,7 +152,7 @@ function c511004014.op(e,tp,eg,ep,ev,re,r,rp)
 		local e11a=e11:Clone()
 		e11a:SetCode(EVENT_BE_MATERIAL)
 		Duel.RegisterEffect(e11a,tp)
-		if Duel.SelectYesNo(tp,aux.Stringid(4013,14)) and Duel.SelectYesNo(1-tp,aux.Stringid(4013,14)) then
+		if Duel.SelectYesNo(tp,aux.Stringid(4013,14)) and Duel.SelectYesNo(tp,aux.Stringid(4013,14)) then
 			Duel.Hint(HINT_MESSAGE,tp,aux.Stringid(4013,15))
 			Duel.Hint(HINT_MESSAGE,1-tp,aux.Stringid(4013,15))
 			--manga rules
@@ -164,28 +165,28 @@ function c511004014.op(e,tp,eg,ep,ev,re,r,rp)
 			Duel.RegisterEffect(e12,tp)
 			local e13=e12:Clone()
 			e13:SetCode(EFFECT_MAX_MZONE)
-			e13:SetValue(c511004014.mvalue)
+			e13:SetValue(s.mvalue)
 			Duel.RegisterEffect(e13,tp)
 			local e14=e12:Clone()
 			e14:SetCode(EFFECT_MAX_SZONE)
-			e14:SetValue(c511004014.svalue)
+			e14:SetValue(s.svalue)
 			Duel.RegisterEffect(e14,tp)
 			local e15=e12:Clone()
 			e15:SetCode(EFFECT_CANNOT_ACTIVATE)
-			e15:SetValue(c511004014.aclimit)
+			e15:SetValue(s.aclimit)
 			Duel.RegisterEffect(e15,tp)
 			local e16=Effect.GlobalEffect()
 			e16:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 			e16:SetCode(EVENT_CHAINING)
-			e16:SetOperation(c511004014.aclimit1)
+			e16:SetOperation(s.aclimit1)
 			Duel.RegisterEffect(e16,tp)
 			local e17=e16:Clone()
 			e17:SetCode(EVENT_CHAIN_NEGATED)
-			e17:SetOperation(c511004014.aclimit2)
+			e17:SetOperation(s.aclimit2)
 			Duel.RegisterEffect(e17,tp)
 			local e18=e16:Clone()
 			e18:SetCode(EVENT_SSET)
-			e18:SetOperation(c511004014.checkop)
+			e18:SetOperation(s.checkop)
 			Duel.RegisterEffect(e18,tp)
 			local e19=Effect.GlobalEffect()
 			e19:SetType(EFFECT_TYPE_FIELD)
@@ -200,7 +201,7 @@ function c511004014.op(e,tp,eg,ep,ev,re,r,rp)
 			Duel.RegisterEffect(e20,tp)
 			local e21=e20:Clone()
 			e21:SetCode(EFFECT_CANNOT_SSET)
-			e21:SetTarget(c511004014.setlimit)
+			e21:SetTarget(s.setlimit)
 			Duel.RegisterEffect(e21,tp)
 			local e22=e19:Clone()
 			e22:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
@@ -223,35 +224,35 @@ function c511004014.op(e,tp,eg,ep,ev,re,r,rp)
 		tc2:RegisterFlagEffect(511004017,0,0,0)
 	end
 end
-function c511004014.ttcon(e,c)
+function s.ttcon(e,c)
 	if c==nil then return true end
 	return Duel.GetTributeCount(c)>=3
 end
-function c511004014.ttop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.ttop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=Duel.SelectTribute(tp,c,3,3)
 	c:SetMaterial(g)
 	Duel.Release(g,REASON_SUMMON+REASON_MATERIAL)
 end
-function c511004014.negop(e,tp,eg,ep,ev,re,r,rp)
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	if re:GetHandler():GetFlagEffect(511000173)>0 and re:IsActiveType(TYPE_TRAP+TYPE_SPELL) then
 		Duel.NegateEffect(ev)
 	end
 end
-function c511004014.negop2(e,tp,eg,ep,ev,re,r,rp)
+function s.negop2(e,tp,eg,ep,ev,re,r,rp)
 	for ec in aux.Next(eg) do
 		ec:RegisterFlagEffect(511000173,0,RESET_CHAIN,1)
 	end
 end
-function c511004014.tgfilter(c,te)
+function s.tgfilter(c,te)
 	return c:IsCanBeEffectTarget(te) and c:IsControler(tp)
 end
-function c511004014.atgfilter(c,ap)
+function s.atgfilter(c,ap)
 	return Duel.GetAttacker():GetAttackableTarget():IsContains(c) and c:IsControler(ap)
 end
-function c511004014.block(e,tp,eg,ep,ev,re,r,rp)
+function s.block(e,tp,eg,ep,ev,re,r,rp)
 	local te,tg=Duel.GetChainInfo(ev+1,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TARGET_CARDS)
 	if te and te~=re and te:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and #tg==1 then
-		local g=eg:Filter(c511004014.tgfilter,nil,te,tg:GetFirst():GetControler())
+		local g=eg:Filter(s.tgfilter,nil,te,tg:GetFirst():GetControler())
 		if #g>0 and Duel.SelectYesNo(g:GetFirst():GetSummonPlayer(),aux.Stringid(68823957,1)) then
 			local p=g:GetFirst():GetSummonPlayer()
 			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TARGET)
@@ -264,7 +265,7 @@ function c511004014.block(e,tp,eg,ep,ev,re,r,rp)
 		and not ac:IsStatus(STATUS_ATTACK_CANCELED) then
 		local ap=1-ac:GetControler()
 		local at=Duel.GetAttackTarget()
-		local ag=eg:Filter(c511004014.atgfilter,nil,ap)
+		local ag=eg:Filter(s.atgfilter,nil,ap)
 		if #ag>0 and Duel.SelectYesNo(ag:GetFirst():GetSummonPlayer(),aux.Stringid(68823957,0)) then
 			local p=ag:GetFirst():GetSummonPlayer()
 			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_ATTACKTARGET)
@@ -278,12 +279,12 @@ function c511004014.block(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c511004014.repfilter(c)
+function s.repfilter(c)
 	return c:GetDestination()==LOCATION_GRAVE and c:GetFlagEffect(511004018)==0 and c:GetControler()~=c:GetOwner()
 end
-function c511004014.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return eg:IsExists(c511004014.repfilter,1,nil) end
-	local g=eg:Filter(c511004014.repfilter,nil)
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(s.repfilter,1,nil) end
+	local g=eg:Filter(s.repfilter,nil)
 	for c in aux.Next(g) do
 		if c:GetReason()&REASON_DESTROY==REASON_DESTROY then
 			c:RegisterFlagEffect(511000173,RESET_CHAIN,0,1)
@@ -292,49 +293,49 @@ function c511004014.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 	return true
 end
-function c511004014.repval(e,c)
+function s.repval(e,c)
 	return c:GetControler()~=c:GetOwner() and c:GetDestination()==LOCATION_GRAVE and c:GetFlagEffect(511004018)==0
 end
-function c511004014.grepcon(e,tp,eg,ep,ev,re,r,rp)
+function s.grepcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(Card.IsStatus,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil,STATUS_LEAVE_CONFIRMED)
 end
-function c511004014.grepop(e,tp,eg,ep,ev,re,r,rp)
+function s.grepop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsStatus,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,STATUS_LEAVE_CONFIRMED)
 	for c in aux.Next(g) do
 		c:CancelToGrave()
 		c:RegisterFlagEffect(STATUS_LEAVE_CONFIRMED,RESET_EVENT+0x1fe0000,0,1)
 	end
 end
-function c511004014.grepfilter(c)
+function s.grepfilter(c)
 	return c:GetFlagEffect(STATUS_LEAVE_CONFIRMED)>0
 end
-function c511004014.grepcon2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(c511004014.grepfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+function s.grepcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.grepfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
 end
-function c511004014.grepop2(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c511004014.grepfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+function s.grepop2(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.grepfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 	for c in aux.Next(g) do
 		c:CancelToGrave(false)
 		Duel.SendtoGrave(c,REASON_RULE,c:GetControler())
 	end
 end
-function c511004014.mvalue(e,fp,rp,r)
+function s.mvalue(e,fp,rp,r)
 	return 5-Duel.GetFieldGroupCount(fp,LOCATION_SZONE,0)
 end
-function c511004014.svalue(e,fp,rp,r)
+function s.svalue(e,fp,rp,r)
 	local ct=5
 	for i=5,7 do
 		if Duel.GetFieldCard(fp,LOCATION_SZONE,i) then ct=ct-1 end
 	end
 	return ct-Duel.GetFieldGroupCount(fp,LOCATION_MZONE,0)
 end
-function c511004014.acfilter(c)
+function s.acfilter(c)
 	return c:GetFlagEffect(EFFECT_TYPE_ACTIVATE)>0
 end
-function c511004014.aclimit(e,re,tp)
+function s.aclimit(e,re,tp)
 	if not re:IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
 	if re:IsActiveType(TYPE_SPELL) and re:GetHandler():IsLocation(LOCATION_HAND) then
-		return Duel.IsExistingMatchingCard(c511004014.acfilter,tp,0xff,0,1,nil)
+		return Duel.IsExistingMatchingCard(s.acfilter,tp,0xff,0,1,nil)
 	end
 	if re:IsActiveType(TYPE_FIELD) then
 		return not Duel.GetFieldCard(tp,LOCATION_SZONE,5) and Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)>4
@@ -343,22 +344,22 @@ function c511004014.aclimit(e,re,tp)
 	end
 	return false
 end
-function c511004014.setlimit(e,c,tp)
+function s.setlimit(e,c,tp)
 	return (c:IsLocation(LOCATION_HAND) and ((c:IsType(TYPE_SPELL) and Duel.GetFlagEffect(tp,TYPE_SPELL)>0)
 		or (c:IsType(TYPE_TRAP) and Duel.GetFlagEffect(tp,TYPE_TRAP)>(Duel.IsPlayerAffectedByEffect(tp,511004017) and 1 or 0))))
 		or (c:IsType(TYPE_FIELD) and not Duel.GetFieldCard(tp,LOCATION_SZONE,5) and Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)>4)
 end
-function c511004014.aclimit1(e,tp,eg,ep,ev,re,r,rp)
+function s.aclimit1(e,tp,eg,ep,ev,re,r,rp)
 	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and re:GetHandler():IsPreviousLocation(LOCATION_HAND) then
 		re:GetHandler():RegisterFlagEffect(EFFECT_TYPE_ACTIVATE,RESET_PHASE+PHASE_END,0,1)
 	end
 end
-function c511004014.aclimit2(e,tp,eg,ep,ev,re,r,rp)
+function s.aclimit2(e,tp,eg,ep,ev,re,r,rp)
 	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and re:GetHandler():IsPreviousLocation(LOCATION_HAND) then
 		re:GetHandler():ResetFlagEffect(EFFECT_TYPE_ACTIVATE)
 	end
 end
-function c511004014.checkop(e,tp,eg,ep,ev,re,r,rp)
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	local hg=eg:Filter(Card.IsPreviousLocation,nil,LOCATION_HAND)
 	if #hg>0 then
 		if hg:IsExists(Card.IsType,1,nil,TYPE_SPELL) then
