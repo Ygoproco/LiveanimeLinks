2d8a817769ac8f0a963c05f13483967ff34b3241
Larry126 <larry126@users.noreply.github.com>
2019-03-18 07:57:21 +0800

    Hundred Eyes Dragon summon fix

--- script/c513000004.lua
+++ script/c513000004.lua
@@ -1,6 +1,7 @@
 --ワンハンドレッド·アイ·ドラゴン (Anime)
 --Hundred Eyes Dragon (Anime)
-function c513000004.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--dark synchro summon
 	c:EnableReviveLimit()
 	aux.AddDarkSynchroProcedure(c,aux.NonTuner(nil),nil,8)
@@ -9,50 +10,50 @@ function c513000004.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_ADD_SETCODE)
-	e1:SetValue(0x301)
+	e1:SetValue(0x601)
 	c:RegisterEffect(e1)
 	--copy  
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_ADJUST)
 	e2:SetRange(LOCATION_MZONE) 
-	e2:SetOperation(c513000004.operation)
+	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
 	--search
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(513000004,0))
+	e3:SetDescription(aux.Stringid(135598,0))
 	e3:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
 	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetCondition(c513000004.thcon)
-	e3:SetTarget(c513000004.thtg)
-	e3:SetOperation(c513000004.thop)
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
 end
-function c513000004.filter(c)
+function s.filter(c)
 	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsType(TYPE_MONSTER)
 end
-function c513000004.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()  
-	local wg=Duel.GetMatchingGroup(c513000004.filter,c:GetControler(),LOCATION_GRAVE,0,nil)
+	local wg=Duel.GetMatchingGroup(s.filter,c:GetControler(),LOCATION_GRAVE,0,nil)
 	local wbc=wg:GetFirst()
 	while wbc do
 		local code=wbc:GetOriginalCode()
 		if c:IsFaceup() and c:GetFlagEffect(code)==0 then
-			c:CopyEffect(code,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,1)
-			c:RegisterFlagEffect(code,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+			c:CopyEffect(code,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,1)
+			c:RegisterFlagEffect(code,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 		end
 		wbc=wg:GetNext()
 	end
 end
-function c513000004.thcon(e,tp,eg,ep,ev,re,r,rp)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsReason(REASON_DESTROY)
 end
-function c513000004.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
 end
-function c513000004.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
