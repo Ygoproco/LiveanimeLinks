1aa7650a3b88fd63f43e1d13a48695c6e7404e93
Larry126 <larry126@users.noreply.github.com>
2019-03-18 07:02:50 +0800

    Add files via upload

--- script/c511000439.lua
+++ script/c511000439.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e1:SetCondition(s.condition)
