50493048044630362be06a1a92990e16b55b20c4
Larry126 <larry126@users.noreply.github.com>
2019-03-25 09:31:23 +0800

    Sauropod Brachion (Anime)

--- script/c511003090.lua
+++ script/c511003090.lua
@@ -1,15 +1,18 @@
---竜脚獣ブラキオン
-function c511003090.initial_effect(c)
+--竜脚獣ブラキオン (Anime)
+--Sauropod Brachion (Anime)
+--fixed by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
 	--turn set
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(41753322,1))
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCategory(CATEGORY_POSITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCondition(c511003090.poscon)
-	e1:SetTarget(c511003090.postg)
-	e1:SetOperation(c511003090.posop)
+	e1:SetCondition(s.poscon)
+	e1:SetTarget(s.postg)
+	e1:SetOperation(s.posop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
@@ -18,16 +21,16 @@ function c511003090.initial_effect(c)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
 end
-function c511003090.poscon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPosition(POS_FACEUP_ATTACK) and eg:IsExists(Card.IsControler,1,nil,1-tp)
+function s.poscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsAttackPos() and eg:IsExists(Card.IsControler,1,nil,1-tp)
 end
-function c511003090.postg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsCanTurnSet() end
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,e:GetHandler(),1,0,0)
 end
-function c511003090.posop(e,tp,eg,ep,ev,re,r,rp)
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsPosition(POS_FACEUP_ATTACK) then
-		Duel.ChangePosition(c,POS_FACEDOWN_DEFENSE)
+		Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
 	end
 end
