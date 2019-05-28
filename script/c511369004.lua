d42bab6410709505f311334f31f7aa8bb785fba3
Belisk <48859882+BlackBelisk@users.noreply.github.com>
2019-05-28 14:42:44 -0400

    Fix "Stormrider Hippogriff"

--- script/c511369004.lua
+++ script/c511369004.lua
@@ -5,12 +5,15 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 	--Send to grave
 	local e2=Effect.CreateEffect(c)
@@ -24,28 +27,44 @@ function s.initial_effect(c)
 	e2:SetOperation(s.tgop)
 	c:RegisterEffect(e2)
 end
-function s.spcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
+function s.szfilter(c)
+	return c:GetSequence()<5
+end
+function s.spcon(e,tp)
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_SZONE,1,nil)
+		and Duel.IsExistingMatchingCard(s.szfilter,tp,0,LOCATION_SZONE,1,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
 function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
-	return r==REASON_LINK and Duel.GetFieldGroupCount(tp,0,LOCATION_SZONE)==1
+	return r==REASON_LINK and Duel.GetMatchingGroupCount(s.tgfilter,tp,0,LOCATION_SZONE,nil)==1
+end
+function s.tgfilter(c)
+	return c:GetSequence()<5 and c:IsAbleToGrave()
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(1-tp,1) 
-		and Duel.GetFieldGroupCount(tp,0,LOCATION_SZONE)==1 and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,0,LOCATION_SZONE,1,nil) end
+		and Duel.IsExistingMatchingCard(s.tgfilter,tp,0,LOCATION_SZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(s.tgfilter,tp,0,LOCATION_SZONE,nil):GetFirst()
+	e:SetLabelObject(g)
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(1)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1-tp,1)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,0)
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
-  	if Duel.GetFieldGroupCount(tp,0,LOCATION_SZONE)~=1 then return end
   	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,0,LOCATION_SZONE,0,1,1,nil)
-	if #g>0 then
+	local g=e:GetLabelObject()
+	if g then
 		if Duel.SendtoGrave(g,REASON_EFFECT)>0 then
 			Duel.Draw(p,d,REASON_EFFECT)
 		end
