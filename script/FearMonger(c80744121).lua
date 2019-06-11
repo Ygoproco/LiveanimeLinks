0e9ec29ab3ce88e4813a7ea38b43638f33054ef4
stani999 <51709549+stani999@users.noreply.github.com>
2019-06-11 21:45:49 +0200

    New Card ID
    new card ID ( non-used)

--- script/FearMonger(c80744121).lua
+++ script/FearMonger(c80744121).lua
@@ -1,44 +1,44 @@
 --Destiny HERO - Fear Monger (Anime)
-function c80744121.initial_effect(c)
+function c123874861.initial_effect(c)
 	--reg
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_BATTLE_DESTROYED)
-	e1:SetOperation(c80744121.regop)
+	e1:SetOperation(c123874861.regop)
 	c:RegisterEffect(e1)
 end
-function c80744121.regop(e,tp,eg,ep,ev,re,r,rp)
+function c123874861.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsReason(REASON_BATTLE) and c:IsLocation(LOCATION_GRAVE) then
 		local e1=Effect.CreateEffect(c)
-		e1:SetDescription(aux.Stringid(80744121,0))
+		e1:SetDescription(aux.Stringid(123874861,0))
 		e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 		e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 		e1:SetRange(LOCATION_GRAVE)
 		e1:SetCountLimit(1)
 		e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
-		e1:SetCondition(c80744121.spcon)
-		e1:SetTarget(c80744121.sptg)
-		e1:SetOperation(c80744121.spop)
+		e1:SetCondition(c123874861.spcon)
+		e1:SetTarget(c123874861.sptg)
+		e1:SetOperation(c123874861.spop)
 		c:RegisterEffect(e1)
 	end
 end
-function c80744121.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c123874861.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
-function c80744121.spfilter(c,e,tp)
-	return c:IsSetCard(0xc008) and c:GetCode()~=80744121 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function c123874861.spfilter(c,e,tp)
+	return c:IsSetCard(0xc008) and c:GetCode()~=123874861 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c80744121.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c80744121.spfilter(chkc,e,tp) end
+function c123874861.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c123874861.spfilter(chkc,e,tp) end
 	if chk==0 then return true end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c80744121.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c123874861.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c80744121.spop(e,tp,eg,ep,ev,re,r,rp)
+function c123874861.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
