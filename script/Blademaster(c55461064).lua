f07e95b7d99727a74eba50ff9bcadc4a5ab1ed22
stani999 <51709549+stani999@users.noreply.github.com>
2019-06-11 21:48:35 +0200

    Update Blademaster(c55461064).lua

--- script/Blademaster(c55461064).lua
+++ script/Blademaster(c55461064).lua
@@ -1,36 +1,36 @@
 --Destiny HERO - Blade Master (Anime)
-function c55461064.initial_effect(c)
+function c474890421.initial_effect(c)
 	--atkup
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(55461064,0))
+	e1:SetDescription(aux.Stringid(474890421,0))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMING_DAMAGE_STEP)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e1:SetCondition(c55461064.atkcon)
-	e1:SetCost(c55461064.atkcost)
-	e1:SetTarget(c55461064.atktg)
-	e1:SetOperation(c55461064.atkop)
+	e1:SetCondition(c474890421.atkcon)
+	e1:SetCost(c474890421.atkcost)
+	e1:SetTarget(c474890421.atktg)
+	e1:SetOperation(c474890421.atkop)
 	c:RegisterEffect(e1)
 end
-function c55461064.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function c474890421.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local ph=Duel.GetCurrentPhase()
 	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE and (ph~=PHASE_DAMAGE or not Duel.IsDamageCalculated())
 end
-function c55461064.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c474890421.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsDiscardable() end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
 end
-function c55461064.filter(c)
+function c474890421.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0xc008)
 end
-function c55461064.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c55461064.filter,tp,LOCATION_MZONE,0,1,nil) end
+function c474890421.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c474890421.filter,tp,LOCATION_MZONE,0,1,nil) end
 end
-function c55461064.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c55461064.filter,tp,LOCATION_MZONE,0,nil)
+function c474890421.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c474890421.filter,tp,LOCATION_MZONE,0,nil)
 	local tc=g:GetFirst()
 	while tc do
 		local e1=Effect.CreateEffect(e:GetHandler())
