3fa713e3411902c3a7c34dcea4d2b72286994385
ClaireStanfield <hirakimitsu1123@gmail.com>
2019-06-13 01:30:47 -0400

    First Fixes Attempt

---
+++ script/c511000893.lua
@@ -0,0 +1,15 @@
+--ヘルカイトプテラ
+--Kaitoptera
+--updated by ClaireStanfield
+local s,id=GetID()
+function s.initial_effect(c)
+	--direct attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DIRECT_ATTACK)
+	e1:SetCondition(s.con)
+	c:RegisterEffect(e1)
+end
+function s.con(e)
+	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_ONFIELD,0)==1
+end
