239aae157f05157999e772152ede48e0b07b28d6
Larry126 <larry126@users.noreply.github.com>
2019-06-22 14:46:04 +0800

    CDB clean up and minor fixes (#480)
    * Delete Anime DB Valkyrie.cdb
    
    * Delete Anime DB New - pyrQ.cdb
    
    * Delete Anime DB New - Larry126.cdb
    
    * Delete Anime DB New - CCM.cdb
    
    * Delete c511600269.lua
    
    * Delete c511600264.lua
    
    * Delete Anime DB RIRA.cdb
    
    * Delete Anime DB CP19.cdb
    
    * Delete Anime DB Allure Queen.cdb
    
    * Delete Anime DB Fixes - Claire Stanfield.cdb
    
    * Delete Anime DB Stormrider.cdb
    
    * SG Pyro Phoenix script update with OCG
    
    * CDB Clean up
    
    * Add files via upload
    
    * CDB Clean up in line with OCG
    
    * CDB Clean up
    
    * Fixes
    
    - Updated due to OCG releases
    - Minor fixes

--- script/c511600194.lua
+++ script/c511600194.lua
@@ -2,46 +2,48 @@
 --Smoke Mosquito (Manga)
 --scripted by Larry126
 --cleaned up by MLD
-function c511600194.initial_effect(c)
+local s,id,alias=GetID()
+function s.initial_effect(c)
+	alias=c:GetOriginalCodeRule()
 	--activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(511600194,0))
+	e1:SetDescription(aux.Stringid(alias,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c511600194.spcon)
-	e1:SetTarget(c511600194.sptg)
-	e1:SetOperation(c511600194.spop)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 	--lvchange
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(44635489,0))
+	e2:SetDescription(aux.Stringid(alias,1))
 	e2:SetCategory(CATEGORY_LVCHANGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetTarget(c511600194.lvtg)
-	e2:SetOperation(c511600194.lvop)
+	e2:SetTarget(s.lvtg)
+	e2:SetOperation(s.lvop)
 	c:RegisterEffect(e2)
 end
-function c511600194.spcon(e,tp,eg,ep,ev,re,r,rp)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetBattleDamage(tp)>0 and Duel.GetAttackTarget()==nil
 end
-function c511600194.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:GetFlagEffect(511600194)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return c:GetFlagEffect(id)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	c:RegisterFlagEffect(511600194,RESET_CHAIN,0,1)
+	c:RegisterFlagEffect(id,RESET_CHAIN,0,1)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
-function c511600194.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local dam=Duel.GetBattleDamage(tp)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e1:SetOperation(c511600194.damop)
+	e1:SetOperation(s.damop)
 	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
 	Duel.RegisterEffect(e1,tp)
 	if dam>1 and c:IsRelateToEffect(e) then
@@ -49,20 +51,20 @@ function c511600194.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 	Duel.SkipPhase(Duel.GetTurnPlayer(),PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE,1)
 end
-function c511600194.damop(e,tp,eg,ep,ev,re,r,rp)
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ChangeBattleDamage(tp,Duel.GetBattleDamage(tp)/2)
 end
-function c511600194.lvfilter(c,lv)
+function s.lvfilter(c,lv)
 	return c:IsFaceup() and c:IsLevelBelow(2147483647) and not c:IsLevel(lv)
 end
-function c511600194.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c511600194.lvfilter,tp,LOCATION_MZONE,0,1,e:GetHandler(),e:GetHandler():GetLevel()) end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.lvfilter,tp,LOCATION_MZONE,0,1,e:GetHandler(),e:GetHandler():GetLevel()) end
 end
-function c511600194.lvop(e,tp,eg,ep,ev,re,r,rp)
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local g=Duel.SelectMatchingCard(tp,c511600194.lvfilter,tp,LOCATION_MZONE,0,1,1,c,c:GetLevel())
+		local g=Duel.SelectMatchingCard(tp,s.lvfilter,tp,LOCATION_MZONE,0,1,1,c,c:GetLevel())
 		local tc=g:GetFirst()
 		if tc then
 			Duel.HintSelection(g)
