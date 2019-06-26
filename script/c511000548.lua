e415b7f9442bb442d9116ff72aab6b56ac1eb2f6
ClaireStanfield <hirakimitsu1123@gmail.com>
2019-06-26 04:05:50 -0400

    Return of the Doomed filter fix

--- script/c511000548.lua
+++ script/c511000548.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.filter(c,e,tp,tid)
-	return c:IsReason(REASON_DESTROY) and c:GetTurnID()==tid-1 or c:GetTurnID()==tid and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsReason(REASON_DESTROY) and (c:GetTurnID()==tid-1 or c:GetTurnID()==tid) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local tid=Duel.GetTurnCount()
