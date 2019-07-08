88a7da1b19495490a2f6bb5149c5985d48b43122
ClaireStanfield <51684310+ClaireStanfield@users.noreply.github.com>
2019-07-08 04:14:12 -0400

    Few new cards (#486)
    * New cards+cdb
    
    * Dragon Master Knight fix
    
    * Rebirth Tablet further correction
    
    * Different Dimension Dragon

--- script/c511002859.lua
+++ script/c511002859.lua
@@ -11,9 +11,9 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(s.atkval)
-	c:RegisterEffect(e2)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
 end
 s.material_setcode={0xdd,0x10cf,0xcf}
 s.listed_names={5405694,23995346}
