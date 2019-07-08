bb277e9095c5ad0473eaf62cc6435711f1cc3a54
ClaireStanfield <hirakimitsu1123@gmail.com>
2019-07-08 04:11:09 -0400

    Different Dimension Dragon

--- script/c511010701.lua
+++ script/c511010701.lua
@@ -14,9 +14,12 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e2:SetValue(1)
+	e2:SetValue(s.ind2)
 	c:RegisterEffect(e2)
 end
 function s.ind1(e,re,rp,c)
 	return not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and re:IsActiveType(TYPE_SPELL+TYPE_TRAP)
+end
+function s.ind2(e,c)
+	return c:IsAttackBelow(1900)
 end
