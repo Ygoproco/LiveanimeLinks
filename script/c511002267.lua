adb02a4429fc3d4534aecf54837974c065cd3ebd
Larry126 <larry126@users.noreply.github.com>
2019-07-12 15:20:17 +0800

    Graveyard of Wandering Souls (Manga) fixes (#502)
    * Graveyard of Wandering Souls (Manga)
    
    Fixed both effect and name
    
    * Add files via upload

--- script/c511002267.lua
+++ script/c511002267.lua
@@ -1,62 +1,74 @@
---Graveyard of Wandering Souls
-function c511002267.initial_effect(c)
-	--activate
+--魂のさまよう墓場 (Manga)
+--Graveyard of Wandering Souls (Manga)
+--Fixed by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(c511002267.target)
+	e1:SetTarget(s.target)
 	c:RegisterEffect(e1)
-	--token
+	--Token
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOKEN+CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCondition(c511002267.spcon)
-	e2:SetTarget(c511002267.sptg)
-	e2:SetOperation(c511002267.spop)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
-function c511002267.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local res,teg,tep,tev,tre,tr,trp=Duel.CheckEvent(EVENT_TO_GRAVE,true)
-	if res and c511002267.spcon(e,tp,teg,tep,tev,tre,tr,trp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if res and s.spcon(e,tp,teg,tep,tev,tre,tr,trp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsPlayerCanSpecialSummonMonster(tp,23116809,0,0x4011,100,100,1,RACE_PYRO,ATTRIBUTE_FIRE) 
 		and Duel.SelectYesNo(tp,94) then
 		e:SetCategory(CATEGORY_TOKEN+CATEGORY_SPECIAL_SUMMON)
-		e:SetOperation(c511002267.spop)
-		c511002267.sptg(e,tp,teg,tep,tev,tre,tr,trp,1)
+		e:SetOperation(s.spop)
+		s.sptg(e,tp,teg,tep,tev,tre,tr,trp,1)
 	else
 		e:SetCategory(0)
 		e:SetOperation(nil)
 	end
 end
-function c511002267.cfilter(c,tp)
-	return c:IsType(TYPE_MONSTER) and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_HAND+LOCATION_MZONE)
+function s.cfilter(c,tp)
+	return c:GetPreviousControler()==tp and (c:IsPreviousLocation(LOCATION_HAND) and c:IsType(TYPE_MONSTER)
+		or c:IsPreviousLocation(LOCATION_MZONE) and c:GetPreviousTypeOnField()&TYPE_MONSTER==TYPE_MONSTER)
 end
-function c511002267.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c511002267.cfilter,1,nil,tp)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
 end
-function c511002267.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+	local ct=eg:FilterCount(s.cfilter,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,ct,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ct,tp,0)
 end
-function c511002267.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsRelateToEffect(e) 
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local ct=eg:FilterCount(s.cfilter,nil,tp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>=ct
+		and (not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or ct==1)
 		and Duel.IsPlayerCanSpecialSummonMonster(tp,23116809,0,0x4011,100,100,1,RACE_PYRO,ATTRIBUTE_FIRE) then
-		local token=Duel.CreateToken(tp,23116809)
-		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UNRELEASABLE_SUM)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetValue(1)
-		e1:SetReset(RESET_EVENT+0x1fe0000)
-		token:RegisterEffect(e1)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_UNRELEASABLE_NONSUM)
-		token:RegisterEffect(e2)
+		for i=1,ct do
+			local token=Duel.CreateToken(tp,23116809)
+			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UNRELEASABLE_SUM)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e1:SetValue(1)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			token:RegisterEffect(e1,true)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+			token:RegisterEffect(e2,true)
+		end
+		Duel.SpecialSummonComplete()
 	end
 end
