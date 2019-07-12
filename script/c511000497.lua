4f10e4261c4654d7e98286a802420c71f8a274a8
ClaireStanfield <51684310+ClaireStanfield@users.noreply.github.com>
2019-07-12 03:53:33 -0400

    Neos Wiseman fix+update/Chimeratech Overdragon update/D Hero Plasma fix (#498)
    * Neos Wiseman/Chimeratech Overdragon
    
    * Plasma fix
    
    * Wiseman/Plasma/Plasma Discharger+D - Burst effect updates
    
    * Wiseman last fix
    
    * Update c511000311.lua

--- script/c511000497.lua
+++ script/c511000497.lua
@@ -1,58 +1,47 @@
 --ネオス・ワイズマン (Anime)
 --Neos Wiseman (Anime)
-function c511000497.initial_effect(c)
+--updated by ClaireStanfield
+local s,id=GetID()
+function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcMix(c,true,true,89943723,aux.FilterBoolFunction(function(c) return c:IsYubel(true) end))
-	--spsummon condition
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(c511000497.splimit)
-	c:RegisterEffect(e1)
 	--damage&recover
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(511000497,0))
-	e3:SetCategory(CATEGORY_DAMAGE+CATEGORY_RECOVER)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_BATTLE_CONFIRM)
-	e3:SetTarget(c511000497.damtg)
-	e3:SetOperation(c511000497.damop)
-	c:RegisterEffect(e3)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DAMAGE+CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_BATTLE_CONFIRM)
+	e1:SetTarget(s.damtg)
+	e1:SetOperation(s.damop)
+	c:RegisterEffect(e1)
 	--
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e4:SetValue(1)
-	c:RegisterEffect(e4)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
 	--special summon
-	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(511000497,0))
-	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e5:SetCode(EVENT_BATTLE_DESTROYED)
-	e5:SetCost(c511000497.cost)
-	e5:SetTarget(c511000497.target)
-	e5:SetOperation(c511000497.operation)
-	c:RegisterEffect(e5)
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_DESTROYED)
+	e3:SetCost(s.cost)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
 	aux.CallToken(420)
 end
-c511000497.listed_names={89943723,78371393}
-c511000497.material_setcode={0x8,0x3008,0x9,0x561}
-function c511000497.splimit(e,se,sp,st)
-	return st&SUMMON_TYPE_FUSION==SUMMON_TYPE_FUSION
-end
-function c511000497.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+s.listed_names={89943723,78371393}
+s.material_setcode={0x8,0x3008,0x9,0x561}
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local bc=e:GetHandler():GetBattleTarget()
 	if chk==0 then return bc end
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,bc:GetAttack())
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,bc:GetDefense())
 end
-function c511000497.damop(e,tp,eg,ep,ev,re,r,rp)
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local bc=e:GetHandler():GetBattleTarget()
 	local atk=bc:GetAttack()
 	local def=bc:GetDefense()
@@ -61,26 +50,27 @@ function c511000497.damop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Damage(1-tp,atk,REASON_EFFECT)
 	Duel.Recover(tp,def,REASON_EFFECT)
 end
-function c511000497.filter(c,e,tp)
+function s.filter(c,e,tp)
 	return c:IsCode(89943723) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c511000497.cfilter(c)
+function s.cfilter(c)
 	return c:IsCode(78371393) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
 end
-function c511000497.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c511000497.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c511000497.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function c511000497.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c511000497.filter(chkc,e,tp) end
-	if chk==0 then return true end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c511000497.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,g:GetCount(),0,0)
 end
-function c511000497.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
