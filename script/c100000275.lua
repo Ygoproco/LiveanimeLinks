89e66f17d7bfc7b88e08dfe66f3ef7bd532700d4
ClaireStanfield <hirakimitsu1123@gmail.com>
2019-07-16 02:02:18 -0400

    Destiny HERO - Celestial/Captain Tenacious

--- script/c100000275.lua
+++ script/c100000275.lua
@@ -1,5 +1,8 @@
---Ｄ－ＨＥＲＯ ディバインガイ
-function c100000275.initial_effect(c)
+--Ｄ－ＨＥＲＯ ディバインガイ (Anime)
+--Destiny HERO - Celestial (Anime)
+--updated by ClaireStanfield
+local s,id=GetID()
+function s.initial_effect(c)
 	--cannot special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
@@ -12,44 +15,44 @@ function c100000275.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e2:SetTarget(c100000275.destg)
-	e2:SetOperation(c100000275.desop)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
 	--damage
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(511001776,0))
+	e3:SetDescription(aux.Stringid(1918087,0))
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCategory(CATEGORY_DAMAGE)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e3:SetCode(100000275)
-	e3:SetTarget(c100000275.damtg)
-	e3:SetOperation(c100000275.damop)
+	e3:SetCode(id)
+	e3:SetTarget(s.damtg)
+	e3:SetOperation(s.damop)
 	c:RegisterEffect(e3)
 end
-function c100000275.desfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_EQUIP)
+function s.desfilter(c)
+	return (c:IsType(TYPE_EQUIP) and (c:IsFaceup() or c:GetEquipTarget()~=nil))
 end
-function c100000275.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and c100000275.desfilter(chkc) and chkc~=e:GetHandler() end
-	if chk==0 then return Duel.IsExistingMatchingCard(c100000275.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and s.desfilter(chkc) and chkc~=e:GetHandler() end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.desfilter,tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c100000275.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.desfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c100000275.desop(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
-		Duel.RaiseSingleEvent(c,100000275,e,r,rp,ep,ev)
+		Duel.RaiseSingleEvent(c,id,e,r,rp,ep,ev)
 	end
 end
-function c100000275.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(500)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
 end
-function c100000275.damop(e,tp,eg,ep,ev,re,r,rp)
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
 end
--- script/c511018024.lua
+++ script/c511018024.lua
@@ -1,6 +1,9 @@
---Ｄ－ＨＥＲＯ ダイハードガイ
+--Ｄ－ＨＥＲＯ ダイハードガイ (Anime)
+--Destiny HERO - Captain Tenacious (Anime)
 --cleaned up by MLD
-function c511018024.initial_effect(c)
+--fixed by ClaireStanfield
+local s,id=GetID()
+function s.initial_effect(c)
 	--
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -8,26 +11,27 @@ function c511018024.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(c511018024.spcon)
-	e1:SetTarget(c511018024.sptg)
-	e1:SetOperation(c511018024.spop)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
-function c511018024.spcon(e,tp,eg,ep,ev,re,r,rp)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
-function c511018024.filter(c,e,tp,tid)
+function s.filter(c,e,tp,tid)
 	return c:GetReason()&0x21==0x21 and c:GetTurnID()==tid-1 and c:IsSetCard(0xc008)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c511018024.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local tid=Duel.GetTurnCount()
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c511018024.filter(chkc,e,tp,tid) end
-	if chk==0 then return Duel.IsExistingTarget(c511018024.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp,tid) end
-	local tg=Duel.SelectTarget(tp,c511018024.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,tid)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc,e,tp,tid) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp,tid) end
+	local tg=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,tid)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tg,1,0,0)
 end
-function c511018024.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
