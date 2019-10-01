bccba8c954ce394bca3fde078882f9615660adba
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2019-09-30 18:00:11 -0700

    Add files via upload

---
+++ script/c511027049.lua
@@ -0,0 +1,41 @@
+--リンク・バースト
+--Link Burst
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DRAW)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.tpfilter(c)
+	return c:IsType(TYPE_LINK)
+end
+function s.opfilter(c)
+	return c:IsType(TYPE_MONSTER)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tpfilter(chkc) end
+	if chk==0 then return  Duel.IsExistingTarget(s.tpfilter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingTarget(s.opfilter,1-tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g1=Duel.SelectTarget(tp,s.tpfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g2=Duel.SelectTarget(tp,s.opfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	g1:Merge(g2)
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g,p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	g=g:Filter(Card.IsRelateToEffect,nil,e)
+	if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)>0 then
+		Duel.Draw(p,d,REASON_EFFECT)
+	end
+end
