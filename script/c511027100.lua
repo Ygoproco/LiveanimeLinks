ed62077f1d9c822e0eee62a4d0672f6961da6a80
Larry126 <larry126@users.noreply.github.com>
2019-10-16 11:25:56 +0800

    Update c511027100.lua

--- script/c511027100.lua
+++ script/c511027100.lua
@@ -3,12 +3,12 @@
 --Scripted by Playmaker 772211
 local s,id=GetID()
 function s.initial_effect(c)
-	--Atk
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e1:SetCountLimit(1,id)--+EFFECT_COUNT_CODE_OATH
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetOperation(s.activate)
@@ -17,8 +17,8 @@ end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
-	if a:IsControler(1-tp) then a=Duel.GetAttackTarget() d=Duel.GetAttacker() end
-	return a and d and a:IsType(TYPE_LINK)
+	if not a:IsControler(tp) then a,d=d,a end
+	return a and d and a:IsControler(tp) and a:IsType(TYPE_LINK) and not d:IsControler(tp)
 end
 function s.costfilter(c)
 	return c:IsRace(RACE_CYBERSE) and c:IsAbleToGraveAsCost()
@@ -33,29 +33,32 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
-    if a:IsRelateToBattle() and a:IsFaceup() and a:IsControler(tp) then
-		local atk=a:GetLink()
-		local e1=Effect.CreateEffect(e:GetHandler())
+	if not a:IsControler(tp) then a,d=d,a end
+	if a:IsRelateToBattle() and a:IsControler(tp) and a:IsFaceup() then
+		--ATK up
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(atk*1000)
+		e1:SetValue(a:GetLink()*1000)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
 		a:RegisterEffect(e1)
+	end
+	if d:IsRelateToBattle() and not d:IsControler(tp) then
 		--Indestructable
-        local e2=Effect.CreateEffect(e:GetHandler())
+		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 		e2:SetValue(1)
-		e2:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
 		d:RegisterEffect(e2)
 		--No damage
-        local e3=Effect.CreateEffect(e:GetHandler())
-	    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	    e3:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	    e3:SetOperation(s.op)
-	    e3:SetReset(RESET_PHASE+PHASE_DAMAGE)
-	    Duel.RegisterEffect(e3,tp)
-	    end
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e3:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+		e3:SetOperation(s.op)
+		e3:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		Duel.RegisterEffect(e3,tp)
+	end
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ChangeBattleDamage(1-tp,0)
