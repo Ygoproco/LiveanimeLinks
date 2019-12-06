485b943b46002d3f5f9db076f64e342744acaf49
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2019-12-06 08:27:12 -0600

    Updated Duelist Kingdom rules card
    https://yugipedia.com/wiki/Duelist_Kingdom
    https://yugioh.fandom.com/wiki/Duelist_Kingdom
    Only one monster is allowed to declare an attack per turn.
    If a player fails to summon any monsters during their turn when they didn't control one, they will automatically lose the duel.

--- script/c511002621.lua
+++ script/c511002621.lua
@@ -55,6 +55,30 @@ function s.init(c)
 	e8:SetCode(EVENT_DESTROYED)
 	e8:SetOperation(s.damop)
 	Duel.RegisterEffect(e8,0)
+   	 --cannot attack
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_FIELD)
+	e9:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e9:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e9:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e9:SetCondition(s.atkcon)
+	e9:SetTarget(s.atktg)
+	Duel.RegisterEffect(e9,0)
+	--check
+	local e10=Effect.CreateEffect(c)
+	e10:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e10:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e10:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e10:SetOperation(s.atkcheckop)
+	e10:SetLabelObject(e9)
+	Duel.RegisterEffect(e10,0)
+	local e11=Effect.CreateEffect(c)
+	e11:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e11:SetCode(EVENT_PHASE+PHASE_END)
+	e11:SetCountLimit(1)
+	e11:SetCondition(s.wincon)
+	e11:SetOperation(s.winop)
+	Duel.RegisterEffect(e11,0)
 end
 function s.limitfilter(c)
 	return c:IsHasEffect(EFFECT_LIMIT_SUMMON_PROC) and c:GetFlagEffect(id)==0
@@ -100,4 +124,29 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 			end
 		end
 	end
+end
+function s.atkcon(e)
+    return e:GetHandler():GetFlagEffect(id-1)~=0
+end
+function s.atktg(e,c)
+    return c:GetFieldID()~=e:GetLabel()
+end
+function s.atkcheckop(e,tp,eg,ep,ev,re,r,rp)
+    if e:GetHandler():GetFlagEffect(id-1)~=0 then return end
+    local fid=eg:GetFirst():GetFieldID()
+    e:GetHandler():RegisterFlagEffect(id-1,RESET_PHASE+PHASE_END,0,1)
+    e:GetLabelObject():SetLabel(fid)
+end
+function s.wincon(e,tp,eg,ep,ev,re,r,rp)
+    local p=Duel.GetTurnPlayer()
+    return Duel.GetFieldGroupCount(p,LOCATION_MZONE,0)==0 and Duel.GetActivityCount(p,ACTIVITY_NORMALSUMMON)==0 
+        and Duel.GetActivityCount(p,ACTIVITY_SPSUMMON)==0 and Duel.GetActivityCount(p,ACTIVITY_FLIPSUMMON)==0
+end
+function s.winop(e,tp,eg,ep,ev,re,r,rp)
+    local WIN_REASON=0x58
+    local p=Duel.GetTurnPlayer()
+    if Duel.GetFieldGroupCount(p,LOCATION_MZONE,0)==0 and Duel.GetActivityCount(p,ACTIVITY_NORMALSUMMON)==0 
+        and Duel.GetActivityCount(p,ACTIVITY_SPSUMMON)==0 and Duel.GetActivityCount(p,ACTIVITY_FLIPSUMMON)==0 then
+        Duel.Win(1-p,WIN_REASON)
+    end
 end
