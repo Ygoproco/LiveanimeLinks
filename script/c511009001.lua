5c981a5dfc5a4c82dc37ad7431a376914553b850
Larry126 <larry126@users.noreply.github.com>
2020-01-18 22:39:17 +0800

    A few updates
    Corrected a typo in Junk Cross
    Updated Number XX and Phantasm Emperor Trilojig with reference to OCG

--- script/c511009001.lua
+++ script/c511009001.lua
@@ -1,41 +1,41 @@
---幻魔帝トリロジーグ
---Phantasm Emperor Trilojig
---updated by Larry126 and MLD
-function c511009001.initial_effect(c)
+--幻魔帝トリロジーグ (Manga)
+--Phantasm Emperor Trilojig (Manga)
+--Updated by Larry126 and MLD
+local s,id,alias=GetID()
+function s.initial_effect(c)
+	alias=c:GetOriginalCodeRule()
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcMixN(c,true,true,aux.FilterBoolFunction(Card.IsLevel,10),3)
-	--spsummon
+	aux.AddFusionProcMixN(c,false,false,aux.FilterBoolFunction(Card.IsLevel,10),3)
+	--damage
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(5114932,0))
+	e1:SetDescription(aux.Stringid(alias,0))
 	e1:SetCategory(CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCondition(c511009001.damcon)
-	e1:SetTarget(c511009001.damtg)
-	e1:SetOperation(c511009001.damop)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCondition(s.damcon)
+	e1:SetTarget(s.damtg)
+	e1:SetOperation(s.damop)
 	c:RegisterEffect(e1)
 end
-function c511009001.filter(c,tp)
+function s.filter(c,tp)
 	return c:IsPreviousLocation(LOCATION_GRAVE) and c:IsControler(tp)
 end
-function c511009001.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c511009001.filter,1,nil,tp)
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil,tp)
 end
-function c511009001.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.nzatk,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.SetTargetPlayer(1-tp)
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and aux.nzatk(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(aux.nzatk,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,aux.nzatk,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
 end
-function c511009001.damop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectMatchingCard(tp,aux.nzatk,tp,0,LOCATION_MZONE,1,1,nil)
-	local tc=g:GetFirst()
-	if tc then
-		Duel.HintSelection(g)
-		local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-		Duel.Damage(p,tc:GetAttack()/2,REASON_EFFECT)
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and aux.nzatk(tc) and tc:IsRelateToEffect(e) then
+		Duel.Damage(1-tp,tc:GetAttack()//2,REASON_EFFECT)
 	end
 end
--- script/c511009002.lua
+++ script/c511009002.lua
@@ -1,112 +1,114 @@
---Ｎｏ．ＸＸ インフィニティ・ダークホープ
---Number XX: Infinity Dark Hope
+--Ｎｏ．ＸＸ インフィニティ・ダークホープ (Manga)
+--Number XX: Utopic Dark Infinity (Manga)
 --fixed and updated by Larry126
-function c511009002.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
+	alias=c:GetOriginalCodeRule()
 	--xyz summon
-	aux.AddXyzProcedure(c,nil,10,3)
 	c:EnableReviveLimit()
+	aux.AddXyzProcedure(c,nil,10,2,nil,nil,99)
 	--Gain LP
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(53701074,0))
+	e1:SetDescription(aux.Stringid(alias,0))
 	e1:SetCategory(CATEGORY_RECOVER)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1)
-	e1:SetTarget(c511009002.target)
-	e1:SetOperation(c511009002.operation)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 	--Battle
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(511009002,0))
+	e2:SetDescription(aux.Stringid(alias,1))
 	e2:SetCategory(CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e2:SetCondition(c511009002.btcon)
-	e2:SetCost(c511009002.btcost)
-	e2:SetTarget(c511009002.bttg)
-	e2:SetOperation(c511009002.btop)
+	e2:SetCondition(s.btcon)
+	e2:SetCost(s.btcost)
+	e2:SetTarget(s.bttg)
+	e2:SetOperation(s.btop)
 	c:RegisterEffect(e2,false,1)
 end
-function c511009002.filter(c)
+function s.filter(c)
 	return c:IsSummonType(SUMMON_TYPE_SPECIAL) and aux.nzatk(c)
 end
-function c511009002.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c511009002.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c511009002.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,c511009002.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
-function c511009002.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if aux.nzatk(tc) and tc:IsRelateToEffect(e) then
 		local c=e:GetHandler()
 		local fid=c:GetFieldID()
-		tc:RegisterFlagEffect(511009002,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1,fid)
+		tc:RegisterFlagEffect(alias,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1,fid)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
 		e1:SetCountLimit(1)
 		e1:SetLabel(fid)
 		e1:SetLabelObject(tc)
-		e1:SetCondition(c511009002.reccon)
-		e1:SetTarget(c511009002.rectg)
-		e1:SetOperation(c511009002.recop)
+		e1:SetCondition(s.reccon)
+		e1:SetTarget(s.rectg)
+		e1:SetOperation(s.recop)
 		Duel.RegisterEffect(e1,tp)
 	end
 end
-function c511009002.reccon(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabelObject():GetFlagEffectLabel(511009002)~=e:GetLabel() then
+function s.reccon(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabelObject():GetFlagEffectLabel(alias)~=e:GetLabel() then
 		e:Reset()
 		return false
 	else return true end
 end
-function c511009002.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,e:GetLabelObject():GetAttack())
 end
-function c511009002.recop(e,tp,eg,ep,ev,re,r,rp)
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
 	Duel.Recover(p,e:GetLabelObject():GetAttack(),REASON_EFFECT)
 end
-function c511009002.btcon(e,tp,eg,ep,ev,re,r,rp)
+function s.btcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetBattleDamage(tp)>0 or Duel.GetBattleDamage(1-tp)>0
 end
-function c511009002.btcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.btcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function c511009002.bttg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetFlagEffect(5110090020)==0 end
-	e:GetHandler():RegisterFlagEffect(5110090020,RESET_CHAIN,0,1)
+function s.bttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetFlagEffect(id)==0 end
+	e:GetHandler():RegisterFlagEffect(id,RESET_CHAIN,0,1)
 end
-function c511009002.btop(e,tp,eg,ep,ev,re,r,rp)
+function s.btop(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e1:SetOperation(c511009002.damop)
+	e1:SetOperation(s.damop)
 	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
 	Duel.RegisterEffect(e1,tp)
 	local e2=Effect.CreateEffect(e:GetHandler())
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_DESTROYED)
-	e2:SetTarget(c511009002.sptg)
-	e2:SetOperation(c511009002.spop)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
 	e2:SetReset(RESET_PHASE+PHASE_DAMAGE)
 	Duel.RegisterEffect(e2,tp)
 end
-function c511009002.damop(e,tp,eg,ep,ev,re,r,rp)
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ChangeBattleDamage(tp,Duel.GetBattleDamage(tp)/2)
 	Duel.ChangeBattleDamage(1-tp,Duel.GetBattleDamage(1-tp)/2)
 end
-function c511009002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and eg:IsExists(Card.IsCanBeSpecialSummoned,1,nil,e,0,tp,false,false) end
 end
-function c511009002.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.SelectEffectYesNo(tp,e:GetHandler()) then return end
 	local tc=eg:FilterSelect(tp,Card.IsCanBeSpecialSummoned,1,1,nil,e,0,tp,false,false):GetFirst()
 	if tc then
