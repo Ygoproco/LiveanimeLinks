a3da75c681c98521acfe63121f51cb538612f2c1
Larry126 <larry126@users.noreply.github.com>
2020-01-18 23:41:32 +0800

    Supreme King Dragon Clear Wing (Anime) fix
    Now Special Summon should work

--- script/c511009517.lua
+++ script/c511009517.lua
@@ -1,6 +1,8 @@
---Supreme King Servant Dragon Clear Wing
---fixed by MLD
-function c511009517.initial_effect(c)
+--覇王眷竜 クリアウィング (Anime)
+--Supreme King Servant Dragon Clear Wing (Anime)
+local s,id,alias=GetID()
+function s.initial_effect(c)
+	alias=c:GetOriginalCodeRule()
 	--synchro summon
 	aux.AddSynchroProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),1,1,aux.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
@@ -9,17 +11,18 @@ function c511009517.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetRange(LOCATION_EXTRA+LOCATION_GRAVE)
-	e1:SetCondition(c511009517.spcon)
-	e1:SetOperation(c511009517.spop)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 	--spsummon success
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetDescription(aux.Stringid(alias,0))
+	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetTarget(c511009517.distg)
-	e2:SetOperation(c511009517.disop)
+	e2:SetTarget(s.distg)
+	e2:SetOperation(s.disop)
 	c:RegisterEffect(e2)
 	--atk limit
 	local e3=Effect.CreateEffect(c)
@@ -27,40 +30,41 @@ function c511009517.initial_effect(c)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,LOCATION_MZONE)
 	e3:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
-	e3:SetValue(c511009517.atlimit)
+	e3:SetValue(s.atlimit)
 	c:RegisterEffect(e3)
 	--Destroy and damage
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(16691074,0))
+	e4:SetDescription(aux.Stringid(alias,1))
+	e4:SetCategory(CATEGORY_DAMAGE+CATEGORY_DESTROY)
 	e4:SetType(EFFECT_TYPE_QUICK_O)
 	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e4:SetCode(EVENT_FREE_CHAIN)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1)
-	e4:SetCondition(c511009517.descon)
-	e4:SetTarget(c511009517.destg)
-	e4:SetOperation(c511009517.desop)
+	e4:SetCondition(s.descon)
+	e4:SetTarget(s.destg)
+	e4:SetOperation(s.desop)
 	c:RegisterEffect(e4)
 	--spsummon
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(95923441,0))
-	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetDescription(aux.Stringid(alias,2))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DISABLE)
 	e5:SetType(EFFECT_TYPE_QUICK_O)
 	e5:SetCode(EVENT_FREE_CHAIN)
 	e5:SetHintTiming(0,TIMING_END_PHASE)
 	e5:SetRange(LOCATION_MZONE)
-	e5:SetCost(c511009517.spcost)
-	e5:SetTarget(c511009517.sptg)
-	e5:SetOperation(c511009517.spop2)
+	e5:SetCost(s.spcost)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop2)
 	c:RegisterEffect(e5)
 end
-function c511009517.spfilter(c,tp)
+function s.spfilter(c,tp)
 	return c:IsControler(1-tp) and c:IsType(TYPE_SYNCHRO) and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
-function c511009517.cfilter(c)
+function s.cfilter(c)
 	return c:IsFaceup() and c:IsCode(13331639)
 end
-function c511009517.rescon(sg,e,tp,mg)
+function s.rescon(sg,e,tp,mg)
 	local c=e:GetHandler()
 	if c:IsLocation(LOCATION_EXTRA) then
 		return Duel.GetLocationCountFromEx(tp,tp,sg,c)>0
@@ -68,7 +72,7 @@ function c511009517.rescon(sg,e,tp,mg)
 		return aux.ChkfMMZ(1)(sg,e,tp,mg)
 	end
 end
-function c511009517.spcon(e,tp,eg,ep,ev,re,r,rp)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local eff={c:GetCardEffect(EFFECT_NECRO_VALLEY)}
 	for _,te in ipairs(eff) do
@@ -77,52 +81,52 @@ function c511009517.spcon(e,tp,eg,ep,ev,re,r,rp)
 	end
 	local g=Duel.GetReleaseGroup(tp):Filter(Card.IsSetCard,nil,0x20f8)
 	local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_SYNCHRO)
-	return pg:GetCount()<=0 and eg:IsExists(c511009517.spfilter,1,nil,tp) and Duel.IsExistingMatchingCard(c511009517.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	return pg:GetCount()<=0 and eg:IsExists(s.spfilter,1,nil,tp) and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 		and g:GetCount()>1 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,true)
-		and aux.SelectUnselectGroup(g,e,tp,2,2,c511009517.rescon,0)
+		and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
 end
-function c511009517.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local g=Duel.GetReleaseGroup(tp):Filter(Card.IsCode,nil,25955164,62340868,98434877)
+	local g=Duel.GetReleaseGroup(tp):Filter(Card.IsSetCard,nil,0x20f8)
 	local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_SYNCHRO)
-	if pg:GetCount()<=0 and g:GetCount()>1 and aux.SelectUnselectGroup(g,e,tp,2,2,c511009517.rescon,0) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,true) 
+	if pg:GetCount()<=0 and g:GetCount()>1 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,true) 
 		and Duel.SelectEffectYesNo(tp,c) then
-		local sg=aux.SelectUnselectGroup(g,e,tp,2,2,c511009517.rescon,1,tp,HINTMSG_RELEASE)
+		local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_RELEASE)
 		Duel.Release(sg,REASON_COST)
 		Duel.SpecialSummon(c,SUMMON_TYPE_SYNCHRO,tp,tp,false,true,POS_FACEUP)
 		c:CompleteProcedure()
 	end
 end
-function c511009517.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.disfilter1,tp,0,LOCATION_MZONE,1,nil) end
 end
-function c511009517.disop(e,tp,eg,ep,ev,re,r,rp)
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(aux.disfilter1,tp,0,LOCATION_MZONE,nil)
 	local tc=g:GetFirst()
 	while tc do
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+0x1fe0000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 		local e2=Effect.CreateEffect(e:GetHandler())
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+0x1fe0000)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e2)
 		tc=g:GetNext()
 	end
 	Duel.Destroy(g,REASON_EFFECT)
 end
-function c511009517.atlimit(e,c)
+function s.atlimit(e,c)
 	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c~=e:GetHandler()
 end
-function c511009517.descon(e,tp,eg,ep,ev,re,r,rp)
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local bc=c:GetBattleTarget()
 	return bc and bc:IsControler(1-tp)
 end
-function c511009517.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
 	local bc=c:GetBattleTarget()
 	if chkc then return chkc==bc end
@@ -142,7 +146,7 @@ function c511009517.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,bc:GetAttack())
 	end
 end
-function c511009517.desop(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local sum=0
 	e:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -162,41 +166,36 @@ function c511009517.desop(e,tp,eg,ep,ev,re,r,rp)
 	end
 	Duel.Damage(1-tp,sum,REASON_EFFECT)
 end
-function c511009517.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToExtraAsCost() end
 	Duel.SendtoDeck(e:GetHandler(),nil,0,REASON_COST)
 end
-function c511009517.spfilter2(c,e,tp)
-	return c:IsFaceup() and c:IsSetCard(0x20f8) and c:IsType(TYPE_PENDULUM) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spfilter2(c,e,tp)
+	return c:IsFaceup() and c:IsSetCard(0x20f8) and c:IsType(TYPE_PENDULUM)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.GetLocationCountFromEx(tp,tp,e:GetHandler(),c)>1
 end
-function c511009517.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ect=c29724053 and Duel.IsPlayerAffectedByEffect(tp,29724053) and c29724053[tp]
-	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,59822133) and (not ect or ect>=2)
-		and Duel.GetLocationCountFromEx(tp,tp,e:GetHandler())>1 and e:GetHandler():GetFlagEffect(511009517)==0
-		and Duel.IsExistingMatchingCard(c511009517.spfilter2,tp,LOCATION_EXTRA,0,2,nil,e,tp) end
-	e:GetHandler():RegisterFlagEffect(511009517,RESET_CHAIN,0,0)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and (not ect or ect>=2)
+		and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_EXTRA,0,2,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_EXTRA)
 end
-function c511009517.filter(c)
+function s.filter(c)
 	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO)
 end
-function c511009517.spop2(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.IsPlayerAffectedByEffect(tp,59822133) or (ect and ect<2) or Duel.GetLocationCountFromEx(tp)<2 then return end
-	local g=Duel.GetMatchingGroup(c511009517.spfilter2,tp,LOCATION_EXTRA,0,nil,e,tp)
-	if g:GetCount()>=2 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g:Select(tp,2,2,nil)
-		if Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)==0 then return end
-		local ag=Duel.GetMatchingGroup(c511009517.filter,tp,0,LOCATION_MZONE,nil)
-		local tc=ag:GetFirst()
-		while tc do
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or (ect and ect<2) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_EXTRA,0,2,2,nil,e,tp)
+	if Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+		for tc in aux.Next(Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)) do
 			local e2=Effect.CreateEffect(e:GetHandler())
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetCode(EFFECT_SET_ATTACK_FINAL)
 			e2:SetValue(0)
-			e2:SetReset(RESET_EVENT+0x1fe0000)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 			tc:RegisterEffect(e2)
-			tc=ag:GetNext()
 		end
 	end
 end
