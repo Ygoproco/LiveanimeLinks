33ce00c946caa2fcb17a9c892dd4369778d191b5
senpaizuri3 <senpaizuri3@gmail.com>
2020-02-28 03:15:40 -0500

    Small fix "Ultra C" (#639)
    `Duel.IsPlayerCanSpecialSummon(tp,SUMMON_TYPE_XYZ,POS_FACEUP,tp)` -> `Duel.IsPlayerCanSpecialSummonMonster(tp,nil,nil,TYPE_XYZ,nil,nil,nil,nil,nil,POS_FACEUP,tp,SUMMON_TYPE_XYZ)`

--- script/c511001064.lua
+++ script/c511001064.lua
@@ -1,25 +1,26 @@
 --Ultra C
-function c511001064.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_CONTROL)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(c511001064.condition)
-	e1:SetTarget(c511001064.target)
-	e1:SetOperation(c511001064.activate)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	--return
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e2:SetCode(EVENT_LEAVE_FIELD_P)
-	e2:SetOperation(c511001064.checkop)
+	e2:SetOperation(s.checkop)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
 	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetOperation(c511001064.retop)
+	e3:SetOperation(s.retop)
 	e3:SetLabelObject(e2)
 	c:RegisterEffect(e3)
 	--Destroy
@@ -27,32 +28,29 @@ function c511001064.initial_effect(c)
 	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e4:SetRange(LOCATION_SZONE)
 	e4:SetCode(EVENT_CONTROL_CHANGED)
-	e4:SetCondition(c511001064.descon)
-	e4:SetOperation(c511001064.desop)
+	e4:SetCondition(s.descon)
+	e4:SetOperation(s.desop)
 	c:RegisterEffect(e4)
 end
-function c511001064.condition(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
 	return tc:GetSummonType()==SUMMON_TYPE_XYZ and tc:IsControler(1-tp) and tc:IsCode(12533811)
 end
-function c511001064.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_XYZ)
 		return pg:GetCount()<=0 and Duel.GetLocationCountFromEx(tp)>0 and Duel.GetFieldGroupCount(tp,0,LOCATION_EXTRA)>0 
-			and Duel.IsPlayerCanSpecialSummon(tp,SUMMON_TYPE_XYZ,POS_FACEUP,tp) end
+			and Duel.IsPlayerCanSpecialSummonMonster(tp,nil,nil,TYPE_XYZ,nil,nil,nil,nil,nil,POS_FACEUP,tp,SUMMON_TYPE_XYZ) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function c511001064.spfilter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsType(TYPE_XYZ) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
 end
-function c511001064.eqlimit(e,c)
-	return e:GetLabelObject()==c
-end
-function c511001064.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_XYZ)
 	if not c:IsRelateToEffect(e) or pg:GetCount()>0 or Duel.GetLocationCountFromEx(tp)<=0 then return end
-	local sg=Duel.GetMatchingGroup(c511001064.spfilter,tp,0,LOCATION_EXTRA,nil,e,tp)
+	local sg=Duel.GetMatchingGroup(s.spfilter,tp,0,LOCATION_EXTRA,nil,e,tp)
 	if sg:GetCount()>0 then
 		Duel.ConfirmCards(tp,sg)
 		local g=sg:Select(tp,1,1,nil):GetFirst()
@@ -64,18 +62,21 @@ function c511001064.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_EQUIP_LIMIT)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+0x1fe0000)
-		e1:SetValue(c511001064.eqlimit)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(s.eqlimit)
 		e1:SetLabelObject(g)
 		c:RegisterEffect(e1)
 	end
 end
-function c511001064.checkop(e,tp,eg,ep,ev,re,r,rp)
+function s.eqlimit(e,c)
+	return e:GetLabelObject()==c
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsDisabled() then
 		e:SetLabel(1)
 	else e:SetLabel(0) end
 end
-function c511001064.retop(e,tp,eg,ep,ev,re,r,rp)
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabelObject():GetLabel()~=0 then return end
 	local tc=e:GetHandler():GetEquipTarget()
 	if tc and tc:IsLocation(LOCATION_MZONE) then
@@ -86,10 +87,10 @@ function c511001064.retop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c511001064.descon(e,tp,eg,ep,ev,re,r,rp)
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetHandler():GetEquipTarget()
 	return tc and eg:IsContains(tc)
 end
-function c511001064.desop(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
 end
